5482594029d03b773eb294f9abc366ff8aa1d370
taku-y <taku.yoshioka.4096@gmail.com>
2020-12-13 00:02:05 +0900

    Impl Sampler trait

--- examples/test1.rs
+++ examples/test1.rs
@@ -1,7 +1,9 @@
 use std::cell::RefCell;
 use ndarray::Array2;
 
-trait Obs {}
+trait Obs {
+    fn new() -> Self;
+}
 
 trait Info {}
 
@@ -17,19 +19,23 @@ trait Policy<E: Env> {
     fn sample(&self, obs: &E::Obs) -> E::Act;
 }
 
-// struct Sampler<E: Env, P: Policy> {
-//     env: E,
-//     pi : P,
-//     obs: RefCell<E::Obs>,
-// }
+struct Sampler<E: Env, P: Policy<E>> {
+    env: E,
+    pi : P,
+    obs: RefCell<E::Obs>,
+}
 
-// impl<E: Env, P: Policy> Sampler<E, P> {
-//     fn sample(&self) {
-//         let o = self.obs.into_inner();
-//         let a = self.pi.sample(&o);
-//         // self.env.step()
-//     }
-// }
+impl<E: Env, P: Policy<E>> Sampler<E, P> {
+    fn new(env: E, pi: P) -> Self {
+        Sampler { env, pi, obs: RefCell::new(E::Obs::new()) }
+    }
+
+    fn sample(&self) {
+        let a = self.pi.sample(&self.obs.borrow());
+        let (o, r, done, info) = self.env.step(&a);
+        self.obs.replace(o);
+    }
+}
 
 // ----------
 
@@ -45,7 +51,11 @@ struct MyEnv {
     state: RefCell<Array2::<f32>>
 }
 
-impl Obs for MyObs {}
+impl Obs for MyObs {
+    fn new() -> Self {
+        MyObs(1.0)
+    }
+}
 
 impl Info for MyInfo {}
 
@@ -77,8 +87,10 @@ impl MyEnv {
 
 fn main() {
     let env = MyEnv::new();
-    let act = MyAct(1.0);
-    let ret = env.step(&act);
+    let pi = MyPolicy {};
+    let sampler = Sampler::new(env, pi);
+
+    sampler.sample();
 
     println!("finished!");
 }
