96e8ad68d5279587824a9ec48f177abf93b8b3c6
taku-y <taku.yoshioka.4096@gmail.com>
2020-12-30 00:46:03 +0900

    WIP: implements dqn

--- src/agents/dqn/dqn.rs
+++ src/agents/dqn/dqn.rs
@@ -1,29 +1,75 @@
+use std::marker::PhantomData;
 use tch::{Tensor, nn::Module, Kind::Float};
-use crate::core::{Policy, Agent, Step};
+use crate::core::{Obs, Policy, Agent, Step, Env};
 use crate::py_gym_env::{PyGymEnv, PyNDArrayObs, PyGymDiscreteAct, PyGymInfo};
 
-pub struct DQN<M: Module + Clone> {
+pub trait ModuleInputAdapter<T: Obs> {
+    fn convert(&self, obs: &T) -> Tensor;
+}
+
+pub trait ModuleOutputAdapter<T> {
+    fn convert(&self, act: &Tensor) -> T;
+}
+
+// pub struct PyNDArrayObsAdapter<E: Env> {
+//     phantom: PhantomData<E>
+// }
+
+// impl<E: Env> ModuleInputAdapter<E> for PyNDArrayObsAdapter<E> {
+//     fn new() -> Self {
+//         Self {
+//             phantom: PhantomData
+//         }
+//     }
+
+//     fn convert(&self, obs: &E::Obs) -> Tensor {
+//         let obs = obs.0.view().to_slice().unwrap();
+//         // let obs: Tensor = Tensor::of_slice(obs);
+//         Tensor::of_slice(obs)
+//     }
+// }
+
+pub struct DQN<E, M, I, O> where
+    E: Env,
+    M: Module + Clone,
+    I: ModuleInputAdapter<E::Obs>,
+    O: ModuleOutputAdapter<E::Act> {
     n_samples_per_opt: usize,
     n_updates_per_opt: usize,
     qnet: M,
     qnet_tgt: M,
+    from_obs: I,
+    into_act: O,
     train: bool,
+    phantom: PhantomData<E>,
 }
 
-impl<M: Module + Clone> DQN<M> {
-    pub fn new(qnet: M, n_samples_per_opt: usize, n_updates_per_opt: usize) -> Self {
+impl<E, M, I, O> DQN<E, M, I, O> where 
+    E: Env,
+    M: Module + Clone,
+    I: ModuleInputAdapter<E::Obs>,
+    O: ModuleOutputAdapter<E::Act> {
+    pub fn new(qnet: M, n_samples_per_opt: usize, n_updates_per_opt: usize,
+               from_obs: I, into_act: O) -> Self {
         let qnet_tgt = qnet.clone();
         DQN {
             n_samples_per_opt,
             n_updates_per_opt,
             qnet,
             qnet_tgt,
-            train: false
+            from_obs,
+            into_act,
+            train: false,
+            phantom: PhantomData,
         }
     }
 }
 
-impl<M: Module + Clone> Policy<PyGymEnv<PyGymDiscreteAct>> for DQN<M> {
+impl<E, M, I, O> Policy<E> for DQN<E, M, I, O> where 
+    E: Env,
+    M: Module + Clone,
+    I: ModuleInputAdapter<E::Obs>,
+    O: ModuleOutputAdapter<E::Act> {
     fn train(&mut self) {
         self.train = true;
     }
@@ -32,23 +78,38 @@ impl<M: Module + Clone> Policy<PyGymEnv<PyGymDiscreteAct>> for DQN<M> {
         self.train = false;
     }
 
-    fn sample(&self, obs: &PyNDArrayObs) -> PyGymDiscreteAct {
-        let obs = obs.0.view().to_slice().unwrap();
-        let obs: Tensor = Tensor::of_slice(obs);
+    // fn sample(&self, obs: &PyNDArrayObs) -> PyGymDiscreteAct {
+    //     let obs = obs.0.view().to_slice().unwrap();
+    //     let obs: Tensor = Tensor::of_slice(obs);
+    //     let a = obs.apply(&self.qnet);
+    //     let a: i32 = if self.train {
+    //         a.softmax(-1, Float)
+    //         .multinomial(1, false)
+    //         .into()
+    //     } else {
+    //         a.argmax(-1, true).into()
+    //     };
+    //     PyGymDiscreteAct::new(a as u32)
+    // }
+    fn sample(&self, obs: &E::Obs) -> E::Act {
+        let obs = self.from_obs.convert(obs);
         let a = obs.apply(&self.qnet);
-        let a: i32 = if self.train {
+        let a = if self.train {
             a.softmax(-1, Float)
             .multinomial(1, false)
-            .into()
         } else {
-            a.argmax(-1, true).into()
+            a.argmax(-1, true)
         };
-        PyGymDiscreteAct::new(a as u32)
+        self.into_act.convert(&a)
     }
 }
 
-impl<M: Module + Clone> Agent<PyGymEnv<PyGymDiscreteAct>> for DQN<M> {
-    fn observe(&self, _step: Step<PyNDArrayObs, PyGymInfo>) -> bool {
-        true
-    }
-}
+// impl<M> Agent<PyGymEnv<PyGymDiscreteAct>> for DQN<E, M, I, O> where
+//     E: Env,
+//     M: Module + Clone,
+//     I: ModuleInputAdapter<E>,
+//     O: ModuleOutputAdapter<E> {
+//     fn observe(&self, _step: Step<PyNDArrayObs, PyGymInfo>) -> bool {
+//         true
+//     }
+// }
