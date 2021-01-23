2b27b2947fbbc2fb9eaf57f91fb677c6361718e8
taku-y <taku.yoshioka.4096@gmail.com>
2021-01-23 15:45:59 +0900

    Make module for Pong

--- src/agents/tch/py_gym_env/mod.rs
+++ src/agents/tch/py_gym_env/mod.rs
@@ -1,8 +1,8 @@
 pub mod obs;
-pub mod obs_filt_pong;
 pub mod act_d;
 pub mod act_c;
 pub mod util;
+pub mod pong;
 pub use obs::{TchPyGymEnvObs, TchPyGymEnvObsBuffer};
 pub use obs_filt_pong::{TchPyGymEnvObsPongFilter, TchPyGymPongObsShape};
 pub use act_d::{TchPyGymEnvDiscreteAct, TchPyGymEnvDiscreteActBuffer};
