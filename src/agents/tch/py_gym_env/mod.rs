62f4935f1f8526c7b62330a0ddfb45770d880a40
taku-y <taku.yoshioka.4096@gmail.com>
2021-01-23 15:47:25 +0900

    Fix module path

--- src/agents/tch/py_gym_env/mod.rs
+++ src/agents/tch/py_gym_env/mod.rs
@@ -4,7 +4,7 @@ pub mod act_c;
 pub mod util;
 pub mod pong;
 pub use obs::{TchPyGymEnvObs, TchPyGymEnvObsBuffer};
-pub use obs_filt_pong::{TchPyGymEnvObsPongFilter, TchPyGymPongObsShape};
 pub use act_d::{TchPyGymEnvDiscreteAct, TchPyGymEnvDiscreteActBuffer};
 pub use act_c::{TchPyGymEnvContinuousAct, TchPyGymEnvContinuousActBuffer,
                 TchPyGymActFilter, RawFilter};
+pub use pong::{TchPyGymEnvObsPongFilter, TchPyGymPongObsShape};
