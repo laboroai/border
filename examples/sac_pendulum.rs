fc9b54afd6c03fc609d48de82d6c3a681f11e2e1
taku-y <taku.yoshioka.4096@gmail.com>
2021-02-16 08:52:59 +0900

    Tweaks on sac_pendulum

--- examples/sac_pendulum.rs
+++ examples/sac_pendulum.rs
@@ -1,9 +1,16 @@
-use std::error::Error;
+use std::{convert::TryFrom, fs::File, iter::FromIterator};
+use serde::Serialize;
+use anyhow::Result;
+use csv::WriterBuilder;
 use tch::nn;
 use pyo3::PyObject;
 
 use lrr::{
-    core::{Trainer, Agent, util::eval, record::NullTrainRecorder},
+    core::{
+        Agent, Trainer,
+        record::{BufferedRecorder, Record, RecordValue, TensorboardRecorder},
+        util::eval_with_recorder
+    }, 
     env::py_gym_env::{
         PyGymEnv, PyGymEnvActFilter,
         obs::{PyGymEnvObs, PyGymEnvObsRawFilter},
@@ -76,8 +83,13 @@ type ActBuffer = TchPyGymEnvContinuousActBuffer<ActShape>;
 struct ActFilter {}
 
 impl PyGymEnvActFilter<Act> for ActFilter {
-    fn filt(&mut self, act: Act) -> PyObject {
-        to_pyobj::<ActShape>(2f32 * act.act)
+    fn filt(&mut self, act: Act) -> (PyObject, Record) {
+        let act_filt = 2f32 * &act.act;
+        let record = Record::from_slice(&[
+            ("act_org", RecordValue::Array1(Vec::from_iter(act.act.iter().cloned()))),
+            ("act_filt", RecordValue::Array1(Vec::from_iter(act_filt.iter().cloned())))
+        ]);
+        (to_pyobj::<ActShape>(act_filt), record)
     }
 }
 
@@ -107,7 +119,34 @@ fn create_env() -> Env {
         .max_steps(Some(200))
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug, Serialize)]
+struct PendulumRecord {
+    episode: usize,
+    step: usize,
+    reward: f32,
+    obs: Vec<f32>,
+    act_org: Vec<f32>,
+    act_filt: Vec<f32>,
+}
+
+impl TryFrom<&Record> for PendulumRecord {
+    type Error = anyhow::Error;
+
+    fn try_from(record: &Record) -> Result<Self> {
+        Ok(Self {
+            episode: record.get_scalar("episode")? as _,
+            step: record.get_scalar("step")? as _,
+            reward: record.get_scalar("reward")?,
+            obs: Vec::from_iter(record.get_array1("obs")?.iter().cloned()),
+            act_org: Vec::from_iter(record.get_array1("act_org")?.iter().cloned()),
+            act_filt: Vec::from_iter(record.get_array1("act_filt")?.iter().cloned()),
+            //     .collect() //map(|v| *v as f64)
+            // )
+        })
+    }
+}
+
+fn main() -> Result<()> {
     env_logger::init();
     tch::manual_seed(42);
 
@@ -121,17 +160,26 @@ fn main() -> Result<(), Box<dyn Error>> {
         .max_opts(200_000)
         .eval_interval(10_000)
         .n_episodes_per_eval(5);
-    let mut recorder = NullTrainRecorder {};
+    let mut recorder = TensorboardRecorder::new("./examples/model/sac_pendulum");
 
     trainer.train(&mut recorder);
-    trainer.get_agent().save("./examples/model/sac_pendulum")?;
+    trainer.get_agent().save("./examples/model/sac_pendulum").unwrap();
 
     let mut env = create_env();
     let mut agent = create_agent();
+    let mut recorder = BufferedRecorder::new();
     env.set_render(true);
-    agent.load("./examples/model/sac_pendulum")?;
+    agent.load("./examples/model/sac_pendulum").unwrap();
     agent.eval();
-    eval(&mut env, &mut agent, 5);
+
+    eval_with_recorder(&mut env, &mut agent, 5, &mut recorder);
+
+    // Vec<_> field in a struct does not support writing a header in csv crate, so disable it.
+    let mut wtr = csv::WriterBuilder::new().has_headers(false)
+        .from_writer(File::create("examples/model/sac_pendulum_eval.csv")?);
+    for record in recorder.iter() {
+        wtr.serialize(PendulumRecord::try_from(record)?)?;
+    }
 
     Ok(())
 }
