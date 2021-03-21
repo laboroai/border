80ff8f9f9313f6fa83b9d2b378a9338113b141cf
taku-y <taku.yoshioka.4096@gmail.com>
2021-03-22 01:05:09 +0900

    Tweaks (#9)

--- examples/sac_ant.rs
+++ examples/sac_ant.rs
@@ -28,17 +28,18 @@ const DIM_OBS: usize = 28;
 const DIM_ACT: usize = 8;
 const LR_ACTOR: f64 = 3e-4;
 const LR_CRITIC: f64 = 3e-4;
+const N_CRITICS: usize = 2;
 const DISCOUNT_FACTOR: f64 = 0.99;
-const BATCH_SIZE: usize = 128;
-const N_TRANSITIONS_WARMUP: usize = 1000;
+const BATCH_SIZE: usize = 256;
+const N_TRANSITIONS_WARMUP: usize = 10_000;
 const N_UPDATES_PER_OPT: usize = 1;
-const TAU: f64 = 0.005;
+const TAU: f64 = 0.02;
 const ALPHA: f64 = 1.0;
-const REWARD_SCALE: f32 = 3.0;
+const REWARD_SCALE: f32 = 5.0;
 const OPT_INTERVAL: OptInterval = OptInterval::Steps(1);
 const MAX_OPTS: usize = 3_000_000;
 const EVAL_INTERVAL: usize = 10_000;
-const REPLAY_BUFFER_CAPACITY: usize = 1_000_000;
+const REPLAY_BUFFER_CAPACITY: usize = 300_000;
 const N_EPISODES_PER_EVAL: usize = 5;
 // const MAX_STEPS_IN_EPISODE: usize = 200;
 
@@ -66,21 +67,20 @@ impl Shape for ActShape {
 
 fn create_actor(device: tch::Device) -> Model1_2 {
     let network_fn = |p: &nn::Path, in_dim, hidden_dim| nn::seq()
-        .add(nn::linear(p / "al1", in_dim as _, 256, Default::default()))
+        .add(nn::linear(p / "al1", in_dim as _, 400, Default::default()))
         .add_fn(|xs| xs.relu())
-        .add(nn::linear(p / "al2", 256, 256, Default::default()))
-        .add_fn(|xs| xs.relu())
-        .add(nn::linear(p / "al3", 256, hidden_dim as _, Default::default()));
-    Model1_2::new(DIM_OBS, 256, DIM_ACT, LR_ACTOR, network_fn, device)
+        .add(nn::linear(p / "al2", 400, hidden_dim as _, Default::default()))
+        .add_fn(|xs| xs.relu());
+    Model1_2::new(DIM_OBS, 300, DIM_ACT, LR_ACTOR, network_fn, device)
 }
 
 fn create_critic(device: tch::Device) -> Model2_1 {
     let network_fn = |p: &nn::Path, in_dim, out_dim| nn::seq()
-        .add(nn::linear(p / "cl1", in_dim as _, 256, Default::default()))
+        .add(nn::linear(p / "cl1", in_dim as _, 400, Default::default()))
         .add_fn(|xs| xs.relu())
-        .add(nn::linear(p / "cl2", 256, 256, Default::default()))
+        .add(nn::linear(p / "cl2", 400, 300, Default::default()))
         .add_fn(|xs| xs.relu())
-        .add(nn::linear(p / "cl3", 256, out_dim as _, Default::default()));
+        .add(nn::linear(p / "cl3", 300, out_dim as _, Default::default()));
         Model2_1::new(DIM_OBS + DIM_ACT, 1, LR_CRITIC, network_fn, device)
 }
 
@@ -95,7 +95,7 @@ type ActBuffer = TchPyGymEnvContinuousActBuffer<ActShape>;
 fn create_agent() -> impl Agent<Env> {
     let device = tch::Device::cuda_if_available();
     let actor = create_actor(device);
-    let critic = create_critic(device);
+    let critics = (0..=N_CRITICS).map(|_| create_critic(device)).collect();
     let replay_buffer = ReplayBuffer::<Env, ObsBuffer, ActBuffer>::new(REPLAY_BUFFER_CAPACITY, 1);
 
     SACBuilder::default()
@@ -107,7 +107,7 @@ fn create_agent() -> impl Agent<Env> {
         .tau(TAU)
         .alpha(ALPHA)
         .reward_scale(REWARD_SCALE)
-        .build(critic, actor, replay_buffer, device)
+        .build(critics, actor, replay_buffer, device)
 }
 
 fn create_env() -> Env {
