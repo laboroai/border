19f7ac6d1b27ebc737f5cfa6b83af0871fb0fcbf
taku-y <taku.yoshioka.4096@gmail.com>
2021-04-23 08:24:56 +0900

    Use smooth l1 loss (#11)

--- src/agent/tch/util/quantile_loss.rs
+++ src/agent/tch/util/quantile_loss.rs
@@ -7,9 +7,10 @@ use tch::Tensor;
 pub fn quantile_huber_loss(x: &Tensor, tau: &Tensor) -> Tensor {
     debug_assert_eq!(x.size().as_slice(), tau.size().as_slice());
 
-    let x_abs = &x.abs();
-    let x_abs_gt_1 = &x_abs.gt(1.0);
+    // let x_abs = &x.abs();
+    // let x_abs_gt_1 = &x_abs.gt(1.0);
     let lt_0 = &x.lt(0.0);
-    let loss = (x_abs - 0.5).where1(x_abs_gt_1, &(0.5 * x.pow(2.0)));
+    // let loss = (x_abs - 0.5).where1(x_abs_gt_1, &(0.5 * x.pow(2.0)));
+    let loss = x.smooth_l1_loss(&Tensor::zeros_like(x), tch::Reduction::None, 1.0);
     (tau - Tensor::where4(lt_0, 1., 0.)).abs() * loss
 }
