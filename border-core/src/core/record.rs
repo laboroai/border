6de8ff4c82be703284b0b5ed004536548442aa75
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2021-05-17 22:09:19 +0900

    Add border-core as a separate crate (#28)

---
+++ border-core/src/core/record.rs
@@ -0,0 +1,191 @@
+//! This module offers types for recording values obtained during training and evaluation.
+//!
+use chrono::prelude::{DateTime, Local};
+use std::{
+    collections::{
+        hash_map::{IntoIter, Iter, Keys},
+        HashMap,
+    },
+    convert::Into,
+    iter::IntoIterator,
+    path::Path,
+};
+use tensorboard_rs::summary_writer::SummaryWriter;
+
+use crate::error::LrrError;
+
+#[derive(Debug, Clone)]
+/// Represents possible types of values in a [`Record`].
+pub enum RecordValue {
+    /// Represents a scalar, e.g., optimization steps and loss value.
+    Scalar(f32),
+
+    /// Represents a datetime.
+    DateTime(DateTime<Local>),
+
+    /// A 1-dimensional array
+    Array1(Vec<f32>),
+}
+
+#[derive(Debug)]
+/// Represents a record.
+pub struct Record(HashMap<String, RecordValue>);
+
+impl Record {
+    /// Construct empty record.
+    pub fn empty() -> Self {
+        Self { 0: HashMap::new() }
+    }
+
+    /// Create `Record` from slice of `(Into<String>, RecordValue)`.
+    pub fn from_slice<K: Into<String> + Clone>(s: &[(K, RecordValue)]) -> Self {
+        Self(
+            s.iter()
+                .map(|(k, v)| (k.clone().into(), v.clone()))
+                .collect(),
+        )
+    }
+
+    /// Get keys.
+    pub fn keys(&self) -> Keys<String, RecordValue> {
+        self.0.keys()
+    }
+
+    /// Insert a key-value pair into the record.
+    pub fn insert(&mut self, k: impl Into<String>, v: RecordValue) {
+        self.0.insert(k.into(), v);
+    }
+
+    /// Return an iterator over key-value pairs in the record.
+    pub fn iter(&self) -> Iter<'_, String, RecordValue> {
+        self.0.iter()
+    }
+
+    /// Return an iterator over key-value pairs in the record.
+    pub fn into_iter_in_record(self) -> IntoIter<String, RecordValue> {
+        self.0.into_iter()
+    }
+
+    /// Get the value of the given key.
+    pub fn get(&self, k: &str) -> Option<&RecordValue> {
+        self.0.get(k)
+    }
+
+    /// Merge records.
+    pub fn merge(self, record: Record) -> Self {
+        Record(self.0.into_iter().chain(record.0).collect())
+    }
+
+    /// Get scalar value.
+    pub fn get_scalar(&self, k: &str) -> Result<f32, LrrError> {
+        if let Some(v) = self.0.get(k) {
+            match v {
+                RecordValue::Scalar(v) => Ok(*v as _),
+                _ => Err(LrrError::RecordValueTypeError("Scalar".to_string())),
+            }
+        } else {
+            Err(LrrError::RecordKeyError(k.to_string()))
+        }
+    }
+
+    /// Get Array1 value.
+    pub fn get_array1(&self, k: &str) -> Result<Vec<f32>, LrrError> {
+        if let Some(v) = self.0.get(k) {
+            match v {
+                RecordValue::Array1(v) => Ok(v.clone()),
+                _ => Err(LrrError::RecordValueTypeError("Array1".to_string())),
+            }
+        } else {
+            Err(LrrError::RecordKeyError(k.to_string()))
+        }
+    }
+}
+
+/// Process records provided with [`Recorder::write`]
+pub trait Recorder {
+    /// Write a record to the [`Recorder`].
+    fn write(&mut self, record: Record);
+}
+
+/// A recorder that ignores any record. This struct is used just for debugging.
+pub struct NullRecorder {}
+
+impl NullRecorder {}
+
+impl Recorder for NullRecorder {
+    /// Discard the given record.
+    fn write(&mut self, _record: Record) {}
+}
+
+/// Write records to TFRecord.
+pub struct TensorboardRecorder {
+    writer: SummaryWriter,
+    step_key: String,
+}
+
+impl TensorboardRecorder {
+    /// Construct a [`TensorboardRecorder`].
+    ///
+    /// TFRecord will be stored in `logdir`.
+    pub fn new<P: AsRef<Path>>(logdir: P) -> Self {
+        Self {
+            writer: SummaryWriter::new(logdir),
+            step_key: "n_opts".to_string(),
+        }
+    }
+}
+
+impl Recorder for TensorboardRecorder {
+    /// Write a given [`Record`] into a TFRecord.
+    ///
+    /// It ignores [`RecordValue::Array1`]
+    fn write(&mut self, record: Record) {
+        // TODO: handle error
+        let step = match record.get(&self.step_key).unwrap() {
+            RecordValue::Scalar(v) => *v as usize,
+            _ => {
+                panic!()
+            }
+        };
+
+        for (k, v) in record.iter() {
+            if *k != self.step_key {
+                match v {
+                    RecordValue::Scalar(v) => self.writer.add_scalar(k, *v as f32, step),
+                    RecordValue::DateTime(_) => {} // discard value
+                    _ => {
+                        unimplemented!()
+                    }
+                };
+            }
+        }
+    }
+}
+
+/// Buffered recorder.
+///
+/// This is used for recording sequences of observation and action
+/// during evaluation runs in [`crate::core::util::eval_with_recorder`].
+#[derive(Default)]
+pub struct BufferedRecorder(Vec<Record>);
+
+impl BufferedRecorder {
+    /// Construct the recorder.
+    pub fn new() -> Self {
+        Self(Vec::default())
+    }
+
+    /// Returns an iterator over the records.
+    pub fn iter(&self) -> std::slice::Iter<Record> {
+        self.0.iter()
+    }
+}
+
+impl Recorder for BufferedRecorder {
+    /// Write a [`Record`] to the buffer.
+    ///
+    /// TODO: Consider if it is worth making the method public.
+    fn write(&mut self, record: Record) {
+        self.0.push(record);
+    }
+}
---
+++ border-core/src/core/util.rs
@@ -0,0 +1,131 @@
+#![allow(clippy::float_cmp)]
+//! Utilities for interaction of agents and environments.
+use crate::core::{
+    record::{Record, RecordValue, Recorder},
+    Env, Obs, Policy, Step,
+};
+use log::info;
+use std::cell::RefCell;
+
+/// Takes an action based on the policy and apply it to the environment.
+///
+/// It returns [crate::core::base::Step] object.
+pub fn sample<E: Env, P: Policy<E>>(
+    env: &mut E,
+    policy: &mut P,
+    obs_prev: &RefCell<Option<E::Obs>>,
+) -> (Step<E>, Record) {
+    let obs = obs_prev
+        .replace(None)
+        .expect("The buffer of the previous observations is not initialized.");
+    let a = policy.sample(&obs);
+    let (step, record) = env.step(&a);
+
+    // Replace observations if the corresponding environments are resetted.
+    let obs_reset = env.reset(Some(&step.is_done)).unwrap();
+    let obs_reset = step.obs.clone().merge(obs_reset, &step.is_done);
+    obs_prev.replace(Some(obs_reset));
+
+    (step, record)
+}
+
+#[cfg_attr(doc, aquamarine::aquamarine)]
+/// Run episodes with a policy and return cumlative rewards.
+///
+/// This function assumes that the environment is non-vectorized or `n_proc`=1.
+///
+/// In this function, the main entities of the library, i.e., an environment ([`super::Env`]),
+/// observation ([`super::Obs`]), action ([`super::Act`]) and policy ([`super::Policy`]),
+/// are interacts as illustrated in the following diagram:
+///
+/// ```mermaid
+/// graph LR
+///     Env --> Obs
+///     Obs --> Policy
+///     Policy --> Act
+///     Act --> Env
+/// ```
+///
+/// By definition of the environment, observations and actions can be modified.
+/// The constructor of [`crate::env::py_gym_env::PyGymEnv`] accepts
+/// [`crate::env::py_gym_env::PyGymEnvObsFilter`] and
+/// [`crate::env::py_gym_env::PyGymEnvActFilter`] for the purpose.
+/// In this case, the interaction of the entities is shown as below
+/// (`PyGymEnvAct` is for discrete or continuous actions in reality):
+///
+/// ```mermaid
+/// graph LR
+///     PyGymEnvObsFilter --> PyGymEnvObs
+///     PyGymEnvObs --> Policy
+///     Policy --> PyGymEnvAct
+///     PyGymEnvAct --> PyGymEnvActFilter
+///
+///    subgraph PyGymEnv
+///        PyGymEnvActFilter --> Py(Python runtime)
+///        Py(Python runtime) --> PyGymEnvObsFilter
+///    end
+/// ```
+pub fn eval<E: Env, P: Policy<E>>(env: &mut E, policy: &mut P, n_episodes: usize) -> Vec<f32> {
+    // TODO: check the maximum number of steps of the environment for evaluation.
+    // If it is infinite, the number of evaluation steps should be given in place of
+    // n_episodes.
+    let mut rs = Vec::new();
+    let obs = env.reset(None).unwrap();
+    let obs_prev = RefCell::new(Some(obs));
+
+    for i in 0..n_episodes {
+        let mut r_sum = 0.0;
+        let mut steps = 0;
+        loop {
+            let (step, _) = sample(env, policy, &obs_prev);
+            r_sum += &step.reward[0];
+            if step.is_done[0] == 1 {
+                break;
+            } else {
+                steps += 1;
+            }
+        }
+        rs.push(r_sum);
+        info!("Episode {:?}, {:?} steps, reward = {:?}", i, steps, r_sum);
+    }
+    rs
+}
+
+/// Run episodes with a policy and recorder.
+///
+/// This method assumes that the environment is non-vectorized or `n_proc`=1.
+pub fn eval_with_recorder<E, P, R>(
+    env: &mut E,
+    policy: &mut P,
+    n_episodes: usize,
+    recorder: &mut R,
+) -> Vec<f32>
+where
+    E: Env,
+    P: Policy<E>,
+    R: Recorder,
+{
+    let mut rs = Vec::new();
+    let obs = env.reset(None).unwrap();
+    let obs_prev = RefCell::new(Some(obs));
+
+    for episode in 0..n_episodes {
+        let mut count_step = 0;
+        let mut r_sum = 0.0;
+        loop {
+            let (step, mut record) = sample(env, policy, &obs_prev);
+            if step.is_done[0] == 1 {
+                break;
+            }
+            r_sum += &step.reward[0];
+
+            record.insert("reward", RecordValue::Scalar(step.reward[0] as _));
+            record.insert("episode", RecordValue::Scalar(episode as _));
+            record.insert("step", RecordValue::Scalar(count_step as _));
+            recorder.write(record);
+            count_step += 1;
+        }
+        rs.push(r_sum);
+    }
+    rs
+}
