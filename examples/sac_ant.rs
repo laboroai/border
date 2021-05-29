e077d34dce652418dea59e25095061315fffd0e3
taku-y <taku.yoshioka.4096@gmail.com>
2021-05-29 20:15:47 +0900

    Fix sac_ant example (#29)

--- examples/sac_ant.rs
+++ examples/sac_ant.rs
@@ -2,8 +2,10 @@ use anyhow::Result;
 use border::{
     agent::{
         tch::{
-            model::{Model1_2, Model2_1},
-            sac::EntCoefMode,
+            model::{SubModel, SubModel2},
+            opt::OptimizerConfig,
+            sac::{Actor, ActorBuilder, Critic, CriticBuilder, EntCoefMode},
+            util::OutDim,
             ReplayBuffer, SACBuilder,
         },
         CriticLoss, OptInterval,
@@ -19,11 +21,15 @@ use border::{
 use border_core::{record::TensorboardRecorder, util, Agent, TrainerBuilder};
 use clap::{App, Arg};
 use log::info;
-use std::time::Duration;
-use tch::nn;
+use serde::{Deserialize, Serialize};
+use std::{default::Default, time::Duration};
+use tch::{
+    nn::{self, Module},
+    Device, Tensor,
+};
 
-const DIM_OBS: usize = 28;
-const DIM_ACT: usize = 8;
+const OBS_DIM: usize = 28;
+const ACT_DIM: usize = 8;
 const LR_ACTOR: f64 = 3e-4;
 const LR_CRITIC: f64 = 3e-4;
 const N_CRITICS: usize = 2;
@@ -33,7 +39,7 @@ const N_TRANSITIONS_WARMUP: usize = 10_000;
 const N_UPDATES_PER_OPT: usize = 1;
 const TAU: f64 = 0.02;
 // const ALPHA: f64 = 1.0;
-const TARGET_ENTROPY: f64 = -(DIM_ACT as f64);
+const TARGET_ENTROPY: f64 = -(ACT_DIM as f64);
 const LR_ENT_COEF: f64 = 3e-4;
 const REWARD_SCALE: f32 = 1.0;
 const CRITIC_LOSS: CriticLoss = CriticLoss::SmoothL1;
@@ -49,7 +55,7 @@ struct ObsShape {}
 
 impl Shape for ObsShape {
     fn shape() -> &'static [usize] {
-        &[DIM_OBS]
+        &[OBS_DIM]
     }
 }
 
@@ -58,7 +64,7 @@ struct ActShape {}
 
 impl Shape for ActShape {
     fn shape() -> &'static [usize] {
-        &[DIM_ACT]
+        &[ACT_DIM]
     }
 
     fn squeeze_first_dim() -> bool {
@@ -66,33 +72,222 @@ impl Shape for ActShape {
     }
 }
 
-fn create_actor(device: tch::Device) -> Model1_2 {
-    let network_fn = |p: &nn::Path, in_dim, hidden_dim| {
-        nn::seq()
-            .add(nn::linear(p / "al1", in_dim as _, 400, Default::default()))
-            .add_fn(|xs| xs.relu())
-            .add(nn::linear(
-                p / "al2",
-                400,
-                hidden_dim as _,
-                Default::default(),
-            ))
-            .add_fn(|xs| xs.relu())
-    };
-    Model1_2::new(DIM_OBS, 300, DIM_ACT, LR_ACTOR, network_fn, device)
+#[allow(clippy::clippy::upper_case_acronyms)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+struct MLPConfig {
+    in_dim: i64,
+    units: Vec<i64>,
+    out_dim: i64,
 }
 
-fn create_critic(device: tch::Device) -> Model2_1 {
-    let network_fn = |p: &nn::Path, in_dim, out_dim| {
-        nn::seq()
-            .add(nn::linear(p / "cl1", in_dim as _, 400, Default::default()))
-            .add_fn(|xs| xs.relu())
-            .add(nn::linear(p / "cl2", 400, 300, Default::default()))
-            .add_fn(|xs| xs.relu())
-            .add(nn::linear(p / "cl3", 300, out_dim as _, Default::default()))
-    };
-    Model2_1::new(DIM_OBS + DIM_ACT, 1, LR_CRITIC, network_fn, device)
+impl MLPConfig {
+    fn new(in_dim: i64, units: Vec<i64>, out_dim: i64) -> Self {
+        Self {
+            in_dim,
+            units,
+            out_dim,
+        }
+    }
 }
+
+impl OutDim for MLPConfig {
+    fn get_out_dim(&self) -> i64 {
+        self.out_dim
+    }
+
+    fn set_out_dim(&mut self, out_dim: i64) {
+        self.out_dim = out_dim;
+    }
+}
+
+#[allow(clippy::clippy::upper_case_acronyms)]
+struct MLP {
+    in_dim: i64,
+    units: Vec<i64>,
+    out_dim: i64,
+    device: Device,
+    seq: nn::Sequential,
+}
+
+fn mlp(var_store: &nn::VarStore, config: &MLPConfig) -> nn::Sequential {
+    let mut seq = nn::seq();
+    let mut in_dim = config.in_dim;
+    let p = &var_store.root();
+
+    for (i, &n) in config.units.iter().enumerate() {
+        seq = seq.add(nn::linear(
+            p / format!("l{}", i),
+            in_dim,
+            n,
+            Default::default(),
+        ));
+        seq = seq.add_fn(|x| x.relu());
+        in_dim = n;
+    }
+
+    seq
+}
+
+impl SubModel2 for MLP {
+    type Config = MLPConfig;
+    type Input1 = Tensor;
+    type Input2 = Tensor;
+    type Output = Tensor;
+
+    fn forward(&self, input1: &Self::Input1, input2: &Self::Input2) -> Self::Output {
+        let input = Tensor::cat(&[input1, input2], -1).to(self.device);
+        self.seq.forward(&input.to(self.device))
+    }
+
+    fn build(var_store: &nn::VarStore, config: Self::Config) -> Self {
+        let units = &config.units;
+        let in_dim = *units.last().unwrap_or(&config.in_dim);
+        let out_dim = config.out_dim;
+        let p = &var_store.root();
+        let seq = mlp(var_store, &config).add(nn::linear(
+            p / format!("l{}", units.len()),
+            in_dim,
+            out_dim,
+            Default::default(),
+        ));
+
+        Self {
+            in_dim: config.in_dim,
+            units: config.units,
+            out_dim: config.out_dim,
+            device: var_store.device(),
+            seq,
+        }
+    }
+
+    fn clone_with_var_store(&self, var_store: &nn::VarStore) -> Self {
+        Self::build(
+            var_store,
+            Self::Config {
+                in_dim: self.in_dim,
+                units: self.units.clone(),
+                out_dim: self.out_dim,
+            },
+        )
+    }
+}
+
+#[allow(clippy::clippy::upper_case_acronyms)]
+struct MLP2 {
+    in_dim: i64,
+    units: Vec<i64>,
+    out_dim: i64,
+    device: Device,
+    head1: nn::Linear,
+    head2: nn::Linear,
+    seq: nn::Sequential,
+}
+
+impl SubModel for MLP2 {
+    type Config = MLPConfig;
+    type Input = Tensor;
+    type Output = (Tensor, Tensor);
+
+    fn forward(&self, input: &Self::Input) -> Self::Output {
+        let x = self.seq.forward(&input.to(self.device));
+        let mean = x.apply(&self.head1);
+        let std = x.apply(&self.head2).exp();
+        (mean, std)
+    }
+
+    fn build(var_store: &nn::VarStore, config: Self::Config) -> Self {
+        let units = config.units;
+        let out_dim = config.out_dim;
+        let device = var_store.device();
+        let mut seq = nn::seq();
+        let mut in_dim = config.in_dim;
+        let p = &var_store.root();
+
+        for (i, &n) in units.iter().enumerate() {
+            seq = seq.add(nn::linear(
+                p / format!("l{}", i),
+                in_dim,
+                n,
+                Default::default(),
+            ));
+            seq = seq.add_fn(|x| x.relu());
+            in_dim = n;
+        }
+
+        let head1 = nn::linear(p / "head1", in_dim, out_dim as _, Default::default());
+        let head2 = nn::linear(p / "head2", in_dim, out_dim as _, Default::default());
+
+        let in_dim = config.in_dim;
+
+        Self {
+            in_dim,
+            units,
+            out_dim,
+            device,
+            head1,
+            head2,
+            seq,
+        }
+    }
+
+    fn clone_with_var_store(&self, var_store: &nn::VarStore) -> Self {
+        let config = Self::Config {
+            in_dim: self.in_dim,
+            units: self.units.clone(),
+            out_dim: self.out_dim,
+        };
+
+        Self::build(var_store, config)
+    }
+}
+
+fn create_actor(device: Device) -> Result<Actor<MLP2>> {
+    let in_dim = 0;
+    let out_dim = 0;
+    let lr_actor = LR_ACTOR;
+    ActorBuilder::default()
+        .pi_config(MLPConfig::new(in_dim, vec![400, 300], out_dim))
+        .opt_config(OptimizerConfig::Adam { lr: lr_actor })
+        .build(device)
+}
+
+fn create_critic(device: Device) -> Result<Critic<MLP>> {
+    let in_dim = 0;
+    let out_dim = 1;
+    let lr_critic = LR_CRITIC;
+    CriticBuilder::default()
+        .q_config(MLPConfig::new(in_dim, vec![400, 300], out_dim))
+        .opt_config(OptimizerConfig::Adam { lr: lr_critic })
+        .build(device)
+}
+
+// fn create_actor(device: tch::Device) -> Model1_2 {
+//     let network_fn = |p: &nn::Path, in_dim, hidden_dim| {
+//         nn::seq()
+//             .add(nn::linear(p / "al1", in_dim as _, 400, Default::default()))
+//             .add_fn(|xs| xs.relu())
+//             .add(nn::linear(
+//                 p / "al2",
+//                 400,
+//                 hidden_dim as _,
+//                 Default::default(),
+//             ))
+//             .add_fn(|xs| xs.relu())
+//     };
+//     Model1_2::new(DIM_OBS, 300, DIM_ACT, LR_ACTOR, network_fn, device)
+// }
+
+// fn create_critic(device: tch::Device) -> Model2_1 {
+//     let network_fn = |p: &nn::Path, in_dim, out_dim| {
+//         nn::seq()
+//             .add(nn::linear(p / "cl1", in_dim as _, 400, Default::default()))
+//             .add_fn(|xs| xs.relu())
+//             .add(nn::linear(p / "cl2", 400, 300, Default::default()))
+//             .add_fn(|xs| xs.relu())
+//             .add(nn::linear(p / "cl3", 300, out_dim as _, Default::default()))
+//     };
+//     Model2_1::new(DIM_OBS + DIM_ACT, 1, LR_CRITIC, network_fn, device)
+// }
 
 type ObsFilter = PyGymEnvObsRawFilter<ObsShape, f32, f32>;
 type ActFilter = PyGymEnvContinuousActRawFilter;
@@ -102,13 +297,15 @@ type Env = PyGymEnv<Obs, Act, ObsFilter, ActFilter>;
 type ObsBuffer = TchPyGymEnvObsBuffer<ObsShape, f32, f32>;
 type ActBuffer = TchPyGymEnvContinuousActBuffer<ActShape>;
 
-fn create_agent() -> impl Agent<Env> {
+fn create_agent() -> Result<impl Agent<Env>> {
     let device = tch::Device::cuda_if_available();
-    let actor = create_actor(device);
-    let critics = (0..N_CRITICS).map(|_| create_critic(device)).collect();
+    let actor = create_actor(device)?;
+    let critics = (0..N_CRITICS)
+        .map(|_| create_critic(device).expect("Cannot create critic"))
+        .collect::<Vec<_>>();
     let replay_buffer = ReplayBuffer::<Env, ObsBuffer, ActBuffer>::new(REPLAY_BUFFER_CAPACITY, 1);
 
-    SACBuilder::default()
+    Ok(SACBuilder::default()
         .opt_interval(OPT_INTERVAL)
         .n_updates_per_opt(N_UPDATES_PER_OPT)
         .min_transitions_warmup(N_TRANSITIONS_WARMUP)
@@ -118,7 +315,7 @@ fn create_agent() -> impl Agent<Env> {
         .ent_coef_mode(EntCoefMode::Auto(TARGET_ENTROPY, LR_ENT_COEF))
         .reward_scale(REWARD_SCALE)
         .critic_loss(CRITIC_LOSS)
-        .build(critics, actor, replay_buffer, device)
+        .build(critics, actor, replay_buffer, device))
 }
 
 fn create_env() -> Env {
@@ -163,7 +360,7 @@ fn main() -> Result<()> {
     if !(matches.is_present("play") || matches.is_present("play-gdrive")) {
         let env = create_env();
         let env_eval = create_env();
-        let agent = create_agent();
+        let agent = create_agent()?;
         let mut trainer = TrainerBuilder::default()
             .max_opts(MAX_OPTS)
             .eval_interval(EVAL_INTERVAL)
@@ -178,7 +375,7 @@ fn main() -> Result<()> {
             .unwrap();
     } else {
         let mut env = create_env();
-        let mut agent = create_agent();
+        let mut agent = create_agent()?;
 
         if matches.is_present("play") {
             let model_dir = matches
