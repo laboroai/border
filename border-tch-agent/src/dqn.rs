a260538ba5efe20c5eccf69e1b35341428bc1330
taku-y <taku.yoshioka.4096@gmail.com>
2021-06-05 14:37:35 +0900

    Tweaks (#35)

--- border-tch-agent/src/dqn.rs
+++ border-tch-agent/src/dqn.rs
@@ -1,8 +1,8 @@
 //! DQN agent.
 mod base;
 mod builder;
-mod explorer;
-mod model;
+pub mod explorer;
+pub mod model;
 pub use base::DQN;
 pub use builder::DQNBuilder;
 pub use explorer::{DQNExplorer, EpsilonGreedy, Softmax};
--- border-tch-agent/src/lib.rs
+++ border-tch-agent/src/lib.rs
@@ -1,8 +1,8 @@
 //! RL agents implemented with [tch](https://crates.io/crates/tch).
-mod util;
-mod opt;
-mod model;
-mod replay_buffer;
-mod dqn;
-mod iqn;
-mod sac;
+pub mod util;
+pub mod opt;
+pub mod model;
+pub mod replay_buffer;
+pub mod dqn;
+pub mod iqn;
+pub mod sac;
