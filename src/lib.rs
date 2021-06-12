00e1cdd1529a64390644b8c0cc3853d70fe7e4e9
taku-y <taku.yoshioka.4096@gmail.com>
2021-06-13 00:54:34 +0900

    Update docstring (#35)

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,155 @@
 #![warn(missing_docs)]
 //! Border is a library for reinforcement learning (RL). The aim of the library is to provide
-//! both environment- and agent-agnostic framework based on the expressiveness and type-safety
-//! of Rust. In order to do this, border provides a function for evaluation ([eval]) and
-//! a trait for training ([Trainer]). These function and trait use some traits consisting
-//! main components in RL such as observation ([Obs]), action ([Act]), and environment ([Env]).
-//! It should be noted that in border, [Policy] represents controllers, which interacts with
-//! environments, while [Agent] provides the interface to train policies by working on [Trainer].
+//! a reference implementation of environments and RL agents, both of which are independent on each other.
+//! For this purpose, this library consists of crates below:
 //!
-//! Using these components, border implements build-in environments and RL algorithms.
-//! [PyGymEnv] and [PyVecGymEnv] are wrapper around [OpenAI gym](https://gym.openai.com)
-//! implemented in Python. These wrappers allow the library to use a lot of environments,
-//! including [atari 2600](https://gym.openai.com/envs/#atari) and
-//! [pybullet-gym](https://github.com/benelot/pybullet-gym), commonly used in the RL comminity.
+//! * [border-core](border_core) provides base components in RL such as 
+//!   [Env](border_core::Env), [Policy](border_core::Policy), [Agent](border_core::Agent),
+//!   [Trainer](border_core::Trainer), [eval](border_core::eval) (function for evaluation).
+//!   It also has utilities like [Record](border_core::record::Record).
+//! * [border-py-gym-env](border_py_gym_env) provides a wrapper of OpenAI gym implemented in Python.
+//!   Its observation ([PyGymEnvObs](border_py_gym_env::PyGymEnvObs)) action 
+//!   ([PyGymEnvDiscreteAct](border_py_gym_env::PyGymEnvDiscreteAct) or
+//!   [PyGymEnvContinuousAct](border_py_gym_env::PyGymEnvContinuousAct)) are based on [ndarray].
+//! * [border-tch-agent](border_tch_agent) is a set of RL agents implemented with [tch].
 //!
-//! Border has some RL agents for discrete action ([DQN]) and continuous action ([SAC]).
-//! These agents use [tch], a wrapper of libtorch.
+//! To bridge between environments and agents, you may need to implement some conversions of
+//! observation and action data. In `dqn_cartpole` example in `example` directory, which uses [border_py_gym_env] and
+//! [border_tch_agent], the following conversions are defined:
 //!
-//! Border enables to make environments and agents be independend regarding their internal
-//! computation. Actually, [PyGymEnv] and [PyVecGymEnv] use [ndarray] in order to represent observation and action.
-//! In contrast, RL agents mentioned above use [tch::Tensor] for internal computation.
-//! Functions and traits in [env::py_gym_env::tch] bridges the gap between these environments and agents.
-//! Like this, border is flexible in combination of environments and agents.
-//! By providing some bridges, it is able to switch computational backends or internal implementation of
-//! environments and agents. This concept is illustrated in schematic diagram of [eval] and [Trainer].
-// pub mod agent;
-// pub mod env;
+//! ```compile_fail
+//! # use border_core::{
+//! #     record::{BufferedRecorder, Record, TensorboardRecorder},
+//! #     shape, util, Agent, TrainerBuilder,
+//! # };
+//! use border_py_gym_env::{newtype_act_d, newtype_obs, PyGymEnv, PyGymEnvDiscreteAct};
+//! # const DIM_OBS: i64 = 4;
+//!
+//! shape!(ObsShape, [DIM_OBS as usize]);
+//! shape!(ActShape, [1]);
+//! newtype_obs!(Obs, ObsFilter, ObsShape, f64, f32);
+//! newtype_act_d!(Act, ActFilter);
+//!
+//! impl From<Obs> for Tensor {
+//!     fn from(obs: Obs) -> Tensor {
+//!         try_from(obs.0.obs).unwrap()
+//!     }
+//! }
+//! 
+//! impl From<Act> for Tensor {
+//!     fn from(act: Act) -> Tensor {
+//!         let v = act.0.act.iter().map(|e| *e as i64).collect::<Vec<_>>();
+//!         let t: Tensor = TryFrom::<Vec<i64>>::try_from(v).unwrap();
+//! 
+//!         // The first dimension of the action tensor is the number of processes,
+//!         // which is 1 for the non-vectorized environment.
+//!         t.unsqueeze(0)
+//!     }
+//! }
+//! 
+//! impl From<Tensor> for Act {
+//!     /// `t` must be a 1-dimentional tensor of `f32`.
+//!     fn from(t: Tensor) -> Self {
+//!         let data: Vec<i64> = t.into();
+//!         let data: Vec<_> = data.iter().map(|e| *e as i32).collect();
+//!         Act(PyGymEnvDiscreteAct::new(data))
+//!     }
+//! }
+//! ```
+//!
+//! The first two lines of code defines the shape of observation and action. It is followed by macros to define
+//! tuple structs named `Obs` and `Act`, which wraps [PyGymEnvObs](border_py_gym_env::PyGymEnvObs) and
+//! [PyGymEnvDiscreteAct](border_py_gym_env::PyGymEnvDiscreteAct), respectively. `ObsFilter` and `ActFilter` are
+//! wrappers of [PyGymEnvObsRawFilter](border_py_gym_env::PyGymEnvObsRawFilter) and 
+//! [PyGymEnvDiscreteActRawFilter](border_py_gym_env::PyGymEnvDiscreteActRawFilter), which pass through
+//! observation and action without any processing. The 4-th argument of `newtype_obs!` macro, `f64`,
+//! means that the cartpole environment outputs its observation as `f64` array. Replacing it with `f32`
+//! will result in a runtime error. The last argument, `f32`, is the type of array in
+//! [PyGymEnvObs](border_py_gym_env::PyGymEnvObs).
+//!
+//! Once `Obs` and `Act` are defined, some [From] traits are implemented on these types.
+//! These are conversions between `Obs`, `Act` and [Tensor]s, as [border_tch_agent] are based on [tch].
+//!
+//! Functions creating the agent and the environment look like below:
+//!
+//! ```compile_fail
+//! fn create_agent() -> Result<impl Agent<Env>> {
+//!     let device = tch::Device::cuda_if_available();
+//!     let qnet = dqn_model::create_dqn_model(DIM_OBS, DIM_ACT, LR_CRITIC, device)?;
+//!     DQNBuilder::default()
+//!         .opt_interval(OPT_INTERVAL)
+//!         .n_updates_per_opt(N_UPDATES_PER_OPT)
+//!         .min_transitions_warmup(N_TRANSITIONS_WARMUP)
+//!         .batch_size(BATCH_SIZE)
+//!         .discount_factor(DISCOUNT_FACTOR)
+//!         .tau(TAU)
+//!         .replay_burffer_capacity(REPLAY_BUFFER_CAPACITY)
+//!         .explorer(DQNExplorer::EpsilonGreedy(EpsilonGreedy::new()))
+//!         .build::<_, _, ObsBuffer, ActBuffer>(qnet, device)
+//! }
+//! 
+//! fn create_env() -> Env {
+//!     let obs_filter = ObsFilter::default();
+//!     let act_filter = ActFilter::default();
+//!     Env::new("CartPole-v0", obs_filter, act_filter, None).unwrap()
+//! }
+//! ```
+//!
+//! Then training
+//!
+//! ```compile_fail
+//!     let env = create_env();
+//!     let env_eval = create_env();
+//!     let agent = create_agent(matches.is_present("egreddy"))?;
+//!     let mut trainer = TrainerBuilder::default()
+//!         .max_opts(MAX_OPTS)
+//!         .eval_interval(EVAL_INTERVAL)
+//!         .n_episodes_per_eval(N_EPISODES_PER_EVAL)
+//!         .model_dir(MODEL_DIR)
+//!         .build(env, env_eval, agent);
+//!     let mut recorder = TensorboardRecorder::new(MODEL_DIR);
+//! 
+//!     trainer.train(&mut recorder);
+//! ```
+//!
+//! and evaluation
+//!
+//! ```compile_fail
+//! let mut env = create_env();
+//! let mut agent = create_agent()?;
+//! let mut recorder = BufferedRecorder::new();
+//! env.set_render(true);
+//! agent.load(MODEL_DIR).unwrap(); // TODO: define appropriate error
+//! agent.eval();
+//! 
+//! util::eval_with_recorder(&mut env, &mut agent, 5, &mut recorder);
+//! ```
+//!
+//! # Features
+//!
+//! ## Core ([border_core])
+//!
+//! * Multiple observations and actions, intended to support vectorized environments.
+//! * Tensorboard support ([TensorboardRecorder](border_core::record::TensorboardRecorder))
+//! * Recording sequences of observation and action ([Record](border_core::record::Record) and
+//!   [BufferedRecorder](border_core::record::BufferedRecorder))
+//!
+//! ## Environments ([border_py_gym_env])
+//!
+//! * Atari using `examples/atari_wrapper.py` and [pybullet-gym](https://github.com/benelot/pybullet-gym)
+//! * Vectorized environment ([PyVecGymEnv](border_py_gym_env::PyVecGymEnv))
+//! * Macros to define newtypes of observation and action ([newtype_obs](border_py_gym_env::newtype_obs),
+//!   [newtype_act_d](border_py_gym_env::newtype_act_d) and [newtype_act_c](border_py_gym_env::newtype_act_c))
+//! * A filter for observation, used in Atari ([FrameStackFilter](border_py_gym_env::FrameStackFilter))
+//!
+//! ## Agents ([border_tch_agent])
+//!
+//! * [DQN](border_tch_agent::dqn::DQN), [IQN](border_tch_agent::iqn::IQN) and [SAC](border_tch_agent::sac::SAC) agents
+//! * Flexible composition of neural networks using [SubModel](border_tch_agent::model::SubModel).
+//! * Model save/load
+//! * [serde] implemented on configurations (e.g., [DQNBuilder](border_tch_agent::dqn::DQNBuilder)).
 pub mod error;
 pub mod util;
-
-// #[allow(unused_imports)]
-// use {
-//     crate::{
-//         agent::tch::{DQN, SAC},
-//         env::py_gym_env::{PyGymEnv, PyVecGymEnv},
-//     },
-//     border_core::{eval, Act, Agent, Env, Obs, Policy, Trainer},
-// };
 
 use ndarray::ArrayD;
 use tch::{TchError, Tensor};
