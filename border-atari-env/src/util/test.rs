35609160900280249e6ce0bea52c2e32947fead0
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2021-12-30 15:57:52 +0900

    Add subbatches for test

--- border-atari-env/src/util/test.rs
+++ border-atari-env/src/util/test.rs
@@ -5,6 +5,7 @@ use crate::{
     BorderAtariObsRawFilter,
 };
 use border_core::{Agent as Agent_, Policy, ReplayBufferBase, replay_buffer::{SimpleReplayBuffer, SubBatch}};
+use std::ptr::copy;
 
 pub type Obs = BorderAtariObs;
 pub type Act = BorderAtariAct;
@@ -15,41 +16,125 @@ pub type ReplayBuffer = SimpleReplayBuffer<ObsBatch, ActBatch>;
 pub type Env = BorderAtariEnv<Obs, Act, ObsFilter, ActFilter>;
 pub type Agent = RandomAgent;
 
+const FRAME_IN_BYTES: usize = 84 * 84;
+
 /// Consists the observation part of a batch in [SimpleReplayBuffer].
 pub struct ObsBatch {
-    pub obs: Vec<u8>,
+    /// The number of samples in the batch.
+    pub n: usize,
+
+    /// The length of a sample in bytes.
+    pub m: usize,
+
+    /// The buffer.
+    pub buf: Vec<u8>,
 }
 
 impl SubBatch for ObsBatch {
-    fn new(_capacity: usize) -> Self {
-        unimplemented!();
+    fn new(capacity: usize) -> Self {
+        let m = 4 * FRAME_IN_BYTES;
+        Self {
+            n: 0,
+            m,
+            buf: vec![0; capacity * m],
+        }
     }
 
-    fn push(&mut self, _i: usize, _data: &Self) {
-        unimplemented!();
+    fn push(&mut self, i: usize, data: &Self) {
+        unsafe {
+            let src: *const u8 = &data.buf[0];
+            let dst: *mut u8 = &mut self.buf[i * self.m];
+            copy(src, dst, self.m);
+        }
     }
 
-    fn sample(&self, _ixs: &Vec<usize>) -> Self {
-        unimplemented!();
+    fn sample(&self, ixs: &Vec<usize>) -> Self {
+        let n = ixs.len();
+        let m = self.m;
+        let mut buf = vec![0; n];
+        (0..n).enumerate().for_each(|(i, ix)| {
+            unsafe {
+                let src: *const u8 = &self.buf[ix];
+                let dst: *mut u8 = &mut buf[i * self.m];
+                copy(src, dst, self.m);
+            }
+        });
+
+        Self {
+            m,
+            n,
+            buf
+        }
+    }
+}
+
+impl From<Obs> for ObsBatch {
+    fn from(obs: Obs) -> Self {
+        Self {
+            n: 1,
+            m: 4 * FRAME_IN_BYTES,
+            buf: obs.frames,
+        }
     }
 }
 
 /// Consists the action part of a batch in [SimpleReplayBuffer].
 pub struct ActBatch {
-    pub obs: Vec<u8>,
+    /// The number of samples in the batch.
+    pub n: usize,
+
+    /// The length of a sample in bytes.
+    pub m: usize,
+
+    /// The buffer.
+    pub buf: Vec<u8>,
 }
 
 impl SubBatch for ActBatch {
-    fn new(_capacity: usize) -> Self {
-        unimplemented!();
+    fn new(capacity: usize) -> Self {
+        let m = 1;
+        Self {
+            n: 0,
+            m,
+            buf: vec![0; capacity * m],
+        }
     }
 
-    fn push(&mut self, _i: usize, _data: &Self) {
-        unimplemented!();
+    fn push(&mut self, i: usize, data: &Self) {
+        unsafe {
+            let src: *const u8 = &data.buf[0];
+            let dst: *mut u8 = &mut self.buf[i * self.m];
+            copy(src, dst, self.m);
+        }
     }
 
-    fn sample(&self, _ixs: &Vec<usize>) -> Self {
-        unimplemented!();
+    fn sample(&self, ixs: &Vec<usize>) -> Self {
+        let n = ixs.len();
+        let m = self.m;
+        let mut buf = vec![0; n];
+        (0..n).enumerate().for_each(|(i, ix)| {
+            unsafe {
+                let src: *const u8 = &self.buf[ix];
+                let dst: *mut u8 = &mut buf[i * self.m];
+                copy(src, dst, self.m);
+            }
+        });
+
+        Self {
+            m,
+            n,
+            buf
+        }
+    }
+}
+
+impl From<Act> for ActBatch {
+    fn from(act: Act) -> Self {
+        Self {
+            n: 1,
+            m: 1,
+            buf: vec![act.act],
+        }
     }
 }
 
