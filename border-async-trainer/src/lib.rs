e3a77700dc697248688b085a5d6fbbbb08af4bb2
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2022-01-02 23:28:22 +0900

    Update docstring

--- border-async-trainer/src/lib.rs
+++ border-async-trainer/src/lib.rs
@@ -1,9 +1,16 @@
 //! Asynchronous trainer using replay buffer.
 //!
-//! # Messages
-//! * From Learner ([Agent](border_core::Agent)) to Actor (`Sampler`)
-//!   - `ModelParams`
-// mod actor;
+//! Training process consists of the following two components:
+//! * [ActorManager] manages [Actor]s, each of which runs a thread for interacting 
+//!   `Agent` and `Env` and taking samples. Those samples will be sent to
+//!   the replay buffer in [AsyncTrainer].
+//! * [AsyncTrainer] is responsible for training of an agent. It also runs a thread
+//!   for pushing samples from [ActorManager] into a replay buffer.
+//!
+//! The `Agent` must implement [SyncModel] trait in order to synchronize the model of
+//! the agent in [Actor] with the trained agent in [AsyncTrainer]. The trait has
+//! the ability to import and export the information of the model as
+//! [SyncModel]`::ModelInfo`.
 mod actor;
 mod actor_manager;
 mod async_trainer;
