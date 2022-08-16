2f7bae5f4c1f1b9b431f7ce991398edf5c79d313
taku-y <taku.yoshioka.4096@gmail.com>
2022-08-16 23:56:56 +0900

    Add eval_with_recorder2()

--- border-core/src/util.rs
+++ border-core/src/util.rs
@@ -90,9 +90,9 @@ use anyhow::Result;
 //     rs
 // }
 
-/// Run episodes with a policy and recorder.
+/// Runs episodes with a policy and recorder.
 ///
-/// It supports non-vectorized environments.
+/// This function does not support vectorized environments.
 pub fn eval_with_recorder<E, P, R>(
     env: &mut E,
     policy: &mut P,
@@ -133,4 +133,51 @@ where
     }
 
     Ok(rs)
+}
+
+/// Runs environment steps with a given policy and recorder.
+///
+/// This function does not support vectorized environments.
+/// 
+/// * `n_steps` - The maximum number of environment steps.
+///   The interaction loop is terminated when is_done is true before reaching `n_steps` environment steps.
+/// * `prev_obs` - The observation, applied to the policy at the first step of interaction.
+///   If `None`, `env.reset_with_index(0)` is invoked.
+/// 
+/// This function returns the sum of rewards during interaction.
+pub fn eval_with_recorder2<E, P, R>(
+    env: &mut E,
+    policy: &mut P,
+    n_steps: usize,
+    recorder: &mut R,
+    prev_obs: Option<<E as Env>::Obs>,
+) -> Result<f32>
+where
+    E: Env,
+    P: Policy<E>,
+    R: Recorder,
+{
+    let mut r_total = 0.0;
+    let mut prev_obs = if prev_obs.is_some() {
+        prev_obs.unwrap()
+    } else {
+        env.reset_with_index(0)?
+    };
+
+    for t in 0..n_steps {
+        let act = policy.sample(&prev_obs);
+        let (step, mut record) = env.step(&act);
+        r_total += step.reward[0];
+        prev_obs = step.obs;
+
+        record.insert("reward", RecordValue::Scalar(step.reward[0] as _));
+        record.insert("step", RecordValue::Scalar(t as _));
+        recorder.write(record);
+
+        if step.is_done[0] == 1 {
+            break;
+        }
+    }
+
+    Ok(r_total)
 }
