c03f9f33f4b4b64fa7403e1bfbf8e16051e5f2ee
taku-y <taku.yoshioka.4096@gmail.com>
2022-10-12 09:25:44 +0900

    Add program to theck pybullet

---
+++ border-py-gym-env/examples/pybullet_pyo3.rs
@@ -0,0 +1,19 @@
+//! This program is used to quickly check pybullet works properly with pyo3.
+
+use anyhow::Result;
+use pyo3::{Python, types::IntoPyDict};
+
+fn main() -> Result<()> {
+    Python::with_gil(|py| {
+        let gym = py.import("gym")?;
+        py.import("pybulletgym")?;
+        let env = gym.call_method("make", ("AntPyBulletEnv-v0",), None)?;
+        let kwargs = vec![("mode", "human")].into_py_dict(py);
+        env.call_method("render", (), Some(kwargs))?;
+
+        env.call_method0("reset")?;
+        loop {}
+
+        Ok(())
+    })
+}
