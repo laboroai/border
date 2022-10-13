79a5ec77c5d9e9c0778adf76429818e30161b8ed
taku-y <taku.yoshioka.4096@gmail.com>
2022-10-13 09:44:53 +0900

    Tweaks for debug

--- border-py-gym-env/examples/pybullet_pyo3.rs
+++ border-py-gym-env/examples/pybullet_pyo3.rs
@@ -1,4 +1,8 @@
 //! This program is used to quickly check pybullet works properly with pyo3.
+use border_py_gym_env::{
+    // PyGymEnv, PyGymEnvActFilter, PyGymEnvConfig, PyGymEnvContinuousAct,
+    //PyGymEnvContinuousActRawFilter, //PyGymEnvObs, PyGymEnvObsFilter, PyGymEnvObsRawFilter,
+};
 
 use anyhow::Result;
 use pyo3::{Python, types::IntoPyDict};
