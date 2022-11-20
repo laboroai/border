bef9a93d91e1474361a5693d42aeacb33c2235ef
taku-y <taku.yoshioka.4096@gmail.com>
2022-11-20 20:15:33 +0900

    Fix python script

--- border-py-gym-env/examples/f32_wrapper.py
+++ border-py-gym-env/examples/f32_wrapper.py
@@ -5,20 +5,24 @@ class F32Wrapper(gym.Wrapper):
     def __init__(self, env):
         gym.Wrapper.__init__(self, env)
 
-    def reset(self):
-        obs = self.env.reset()
+    def reset(self, **kwargs):
+        """Resets the environment.
+
+        Currently it assumes box observation.
+        """
+        obs = self.env.reset(**kwargs)
         if type(obs) == np.ndarray and obs.dtype == np.float64:
             obs = np.array(obs, dtype=np.float32)
 
         return obs
 
     def step(self, act):
-        (obs, reward, done, info) = self.env.step(act)
+        (obs, reward, terminated, truncated, info) = self.env.step(act)
         if type(obs) == np.ndarray and obs.dtype == np.float64:
             obs = np.array(obs, dtype=np.float32)
 
-        return (obs, reward, done, info)
+        return (obs, reward, terminated, truncated, info)
 
-def make_f32(env_name):
-    env = gym.make(env_name)
+def make_f32(env_name, render_mode=None):
+    env = gym.make(env_name, render_mode=render_mode)
     return F32Wrapper(env)
