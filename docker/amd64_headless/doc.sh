fdf85337fff603e142b518b8472e1a6eb0af384e
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2023-01-15 20:49:14 +0900

    Add docker files

---
+++ docker/amd64_headless/doc.sh
@@ -0,0 +1,6 @@
+docker run -it --rm \
+    --name border_headless \
+    --shm-size=512m \
+    --volume="$(pwd)/../..:/home/ubuntu/border" \
+    border_headless bash -l -c \
+    "CARGO_TARGET_DIR=/home/ubuntu/target cargo doc --no-deps --document-private-items; cp -r /home/ubuntu/target/doc ."
