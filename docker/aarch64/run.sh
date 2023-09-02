72eaeee80e165d838a0f954a63475912559e6cea
taku-y <taku.yoshioka.4096@gmail.com>
2023-09-02 21:48:52 +0900

    Update Dockerfile

--- docker/aarch64/run.sh
+++ docker/aarch64/run.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
-# nvidia-docker run -it --rm \
-#     --env="DISPLAY" \
-#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#     --volume="/home/taku-y:/home/taku-y" \
-#     --name my_pybullet my_pybullet bash
-
 docker run -td  \
     --name border \
-    -p 6080:80 \
+    -p 6080:6080 \
     --shm-size=512m \
-    --volume="$(pwd)/../..:/home/ubuntu/border" \
+    --volume="$(pwd)/../..:/root/border" \
     border
