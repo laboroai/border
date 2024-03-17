ee68a11e615382a2e06b0f6db468db3803e18288
taku-y <taku.yoshioka.4096@gmail.com>
2024-03-17 16:46:36 +0900

    Tweaks on Docker files

--- docker/aarch64_doc/doc.sh
+++ docker/aarch64_doc/doc.sh
@@ -2,5 +2,5 @@ docker run -it --rm \
     --name border_headless \
     --shm-size=512m \
     --volume="$(pwd)/../..:/home/ubuntu/border" \
-    border_doc bash -l -c \
-    "CARGO_TARGET_DIR=/home/ubuntu/target cargo doc --no-deps --document-private-items; cp -r /home/ubuntu/target/doc ."
+    border_headless bash -l -c \
+    "cd /home/ubuntu/border; CARGO_TARGET_DIR=/home/ubuntu/target cargo doc --no-deps --document-private-items; cp -r /home/ubuntu/target/doc ."
