7d59bece7ebf4f1f50c2ce7865b909b51fe5c142
taku-y <taku.yoshioka.4096@gmail.com>
2024-06-22 12:39:25 +0900

    Remove subbatch.rs (#18)

--- border-core/src/replay_buffer/batch.rs
+++ border-core/src/replay_buffer/batch.rs
@@ -1,18 +1,17 @@
 //! A generic implementation of [`TransitionBatch`](crate::TransitionBatch).
-use super::BatchBase;
 use crate::TransitionBatch;
 
-// /// A generic implementation of a batch of items.
-// pub trait SubBatch {
-//     /// Builds a subbatch with a capacity.
-//     fn new(capacity: usize) -> Self;
+/// A generic implementation of a batch of items.
+pub trait BatchBase {
+    /// Builds a subbatch with a capacity.
+    fn new(capacity: usize) -> Self;
 
-//     /// Pushes the samples in `data`.
-//     fn push(&mut self, ix: usize, data: Self);
+    /// Pushes the samples in `data`.
+    fn push(&mut self, ix: usize, data: Self);
 
-//     /// Takes samples in the batch.
-//     fn sample(&self, ixs: &Vec<usize>) -> Self;
-// }
+    /// Takes samples in the batch.
+    fn sample(&self, ixs: &Vec<usize>) -> Self;
+}
 
 /// A generic implementation of [`TransitionBatch`](`crate::TransitionBatch`).
 pub struct StdBatch<O, A>
