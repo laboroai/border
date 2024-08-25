02e32b42d1f59ec8169149fea753e7c652a96a60
taku-y <taku.yoshioka.4096@gmail.com>
2024-08-25 23:36:10 +0900

    Fix sac_mujoco_async_tch

--- border/examples/mujoco/sac_mujoco_async_tch.rs
+++ border/examples/mujoco/sac_mujoco_async_tch.rs
@@ -1,16 +1,18 @@
 use anyhow::Result;
 use border_async_trainer::{
-    actor_stats_fmt, ActorManager as ActorManager_, ActorManagerConfig,
-    AsyncTrainer as AsyncTrainer_, AsyncTrainerConfig,
+    util::train_async, /*ActorManager as ActorManager_,*/ ActorManagerConfig,
+    /*AsyncTrainer as AsyncTrainer_,*/ AsyncTrainerConfig,
 };
 use border_core::{
-    replay_buffer::{
+    generic_replay_buffer::{
         SimpleReplayBuffer, SimpleReplayBufferConfig, SimpleStepProcessor,
         SimpleStepProcessorConfig,
     },
+    record::AggregateRecorder,
     DefaultEvaluator,
 };
-use border_derive::SubBatch;
+use border_derive::BatchBase;
+use border_mlflow_tracking::MlflowTrackingClient;
 use border_py_gym_env::{
     util::{arrayd_to_tensor, tensor_to_arrayd},
     ArrayObsFilter, ContinuousActFilter, GymActFilter, GymEnv, GymEnvConfig, GymObsFilter,
@@ -23,45 +25,36 @@ use border_tch_agent::{
     TensorBatch,
 };
 use border_tensorboard::TensorboardRecorder;
-use clap::{App, Arg, ArgMatches};
-use crossbeam_channel::unbounded;
+use clap::Parser;
 use ndarray::{ArrayD, IxDyn};
-use std::{
-    convert::TryFrom,
-    default::Default,
-    sync::{Arc, Mutex},
-};
+use std::{convert::TryFrom, default::Default};
 use tch::Tensor;
 
-type PyObsDtype = f32;
-
-const DIM_OBS: i64 = 27;
-const DIM_ACT: i64 = 8;
 const LR_ACTOR: f64 = 3e-4;
 const LR_CRITIC: f64 = 3e-4;
 const BATCH_SIZE: usize = 256;
-const N_TRANSITIONS_WARMUP: usize = 10_000;
+const WARMUP_PERIOD: usize = 10_000;
 const MAX_OPTS: usize = 3_000_000;
-const EVAL_INTERVAL: usize = 10_000;
-const RECORD_INTERVAL: usize = 10_000;
+const EVAL_INTERVAL: usize = 5_000;
 const SAVE_INTERVAL: usize = 500_000;
 const REPLAY_BUFFER_CAPACITY: usize = 300_000;
-const SYNC_INTERVAL: usize = 100;
-const EVAL_EPISODES: usize = 5;
 const N_CRITICS: usize = 2;
 const TAU: f64 = 0.02;
-const TARGET_ENTROPY: f64 = -(DIM_ACT as f64);
 const LR_ENT_COEF: f64 = 3e-4;
 const CRITIC_LOSS: CriticLoss = CriticLoss::SmoothL1;
-const MODEL_DIR: &str = "./border/examples/mujoco/model/tch/sac_ant_async";
+const MODEL_DIR_BASE: &str = "./border/examples/mujoco/model/tch";
 
-mod obs {
+fn cuda_if_available() -> tch::Device {
+    tch::Device::cuda_if_available()
+}
+
+mod obs_act_types {
     use super::*;
 
     #[derive(Clone, Debug)]
     pub struct Obs(ArrayD<f32>);
 
-    #[derive(Clone, SubBatch)]
+    #[derive(Clone, BatchBase)]
     pub struct ObsBatch(TensorBatch);
 
     impl border_core::Obs for Obs {
@@ -92,10 +85,6 @@ mod obs {
             Self(TensorBatch::from_tensor(tensor))
         }
     }
-}
-
-mod act {
-    use super::*;
 
     #[derive(Clone, Debug)]
     pub struct Act(ArrayD<f32>);
@@ -121,7 +110,7 @@ mod act {
         }
     }
 
-    #[derive(SubBatch)]
+    #[derive(BatchBase)]
     pub struct ActBatch(TensorBatch);
 
     impl From<Act> for ActBatch {
@@ -130,189 +119,210 @@ mod act {
             Self(TensorBatch::from_tensor(tensor))
         }
     }
+
+    type PyObsDtype = f32;
+    pub type ObsFilter = ArrayObsFilter<PyObsDtype, f32, Obs>;
+    pub type ActFilter = ContinuousActFilter<Act>;
+    pub type EnvConfig = GymEnvConfig<Obs, Act, ObsFilter, ActFilter>;
+    pub type Env = GymEnv<Obs, Act, ObsFilter, ActFilter>;
+    pub type StepProc = SimpleStepProcessor<Env, ObsBatch, ActBatch>;
+    pub type ReplayBuffer = SimpleReplayBuffer<ObsBatch, ActBatch>;
+    pub type Evaluator = DefaultEvaluator<Env, Sac<Env, Mlp, Mlp2, ReplayBuffer>>;
 }
 
-use act::{Act, ActBatch};
-use obs::{Obs, ObsBatch};
-
-type ObsFilter = ArrayObsFilter<PyObsDtype, f32, Obs>;
-type ActFilter = ContinuousActFilter<Act>;
-type Env = GymEnv<Obs, Act, ObsFilter, ActFilter>;
-type EnvConfig = GymEnvConfig<Obs, Act, ObsFilter, ActFilter>;
-type StepProc = SimpleStepProcessor<Env, ObsBatch, ActBatch>;
-type ReplayBuffer = SimpleReplayBuffer<ObsBatch, ActBatch>;
-type Agent = Sac<Env, Mlp, Mlp2, ReplayBuffer>;
-type AgentConfig = SacConfig<Mlp, Mlp2>;
-type ActorManager = ActorManager_<Agent, Env, ReplayBuffer, StepProc>;
-type AsyncTrainer = AsyncTrainer_<Agent, Env, ReplayBuffer>;
-type Evaluator = DefaultEvaluator<Env, Agent>;
+use obs_act_types::*;
 
 mod config {
+    use serde::Serialize;
+
     use super::*;
 
-    pub fn env_config() -> GymEnvConfig<Obs, Act, ObsFilter, ActFilter> {
+    pub fn env_config(env_name: &str) -> EnvConfig {
         GymEnvConfig::<Obs, Act, ObsFilter, ActFilter>::default()
-            .name("Ant-v4".to_string())
+            .name(env_name.to_string())
             .obs_filter_config(ObsFilter::default_config())
             .act_filter_config(ActFilter::default_config())
     }
 
-    pub fn agent_config(in_dim: i64, out_dim: i64) -> AgentConfig {
+    pub fn create_async_trainer_config(model_dir: &str) -> Result<AsyncTrainerConfig> {
+        Ok(AsyncTrainerConfig::default()
+            .model_dir(model_dir)?
+            .max_opts(MAX_OPTS)?
+            .eval_interval(EVAL_INTERVAL)?
+            .flush_record_interval(EVAL_INTERVAL)?
+            .record_compute_cost_interval(EVAL_INTERVAL)?
+            .sync_interval(1)?
+            .save_interval(SAVE_INTERVAL)?
+            .warmup_period(WARMUP_PERIOD)?)
+    }
+
+    pub fn create_sac_config(dim_obs: i64, dim_act: i64, target_ent: f64) -> SacConfig<Mlp, Mlp2> {
+        let device = cuda_if_available();
         let actor_config = ActorConfig::default()
             .opt_config(OptimizerConfig::Adam { lr: LR_ACTOR })
-            .out_dim(out_dim)
-            .pi_config(MlpConfig::new(in_dim, vec![400, 300], out_dim, false));
+            .out_dim(dim_act)
+            .pi_config(MlpConfig::new(dim_obs, vec![400, 300], dim_act, false));
         let critic_config = CriticConfig::default()
             .opt_config(OptimizerConfig::Adam { lr: LR_CRITIC })
-            .q_config(MlpConfig::new(in_dim + out_dim, vec![400, 300], 1, false));
+            .q_config(MlpConfig::new(dim_obs + dim_act, vec![400, 300], 1, false));
 
         SacConfig::default()
             .batch_size(BATCH_SIZE)
-            .min_transitions_warmup(N_TRANSITIONS_WARMUP)
             .actor_config(actor_config)
             .critic_config(critic_config)
             .tau(TAU)
             .critic_loss(CRITIC_LOSS)
             .n_critics(N_CRITICS)
-            .ent_coef_mode(EntCoefMode::Auto(TARGET_ENTROPY, LR_ENT_COEF))
+            .ent_coef_mode(EntCoefMode::Auto(target_ent, LR_ENT_COEF))
+            .device(device)
     }
 
-    pub fn async_trainer_config() -> AsyncTrainerConfig {
-        AsyncTrainerConfig {
-            model_dir: Some(MODEL_DIR.to_string()),
-            record_interval: RECORD_INTERVAL,
-            eval_interval: EVAL_INTERVAL,
-            max_train_steps: MAX_OPTS,
-            save_interval: SAVE_INTERVAL,
-            sync_interval: SYNC_INTERVAL,
+    pub fn show_config(
+        env_config: &EnvConfig,
+        agent_config: &SacConfig<Mlp, Mlp2>,
+        actor_man_config: &ActorManagerConfig,
+        trainer_config: &AsyncTrainerConfig,
+    ) {
+        println!("Device: {:?}", tch::Device::cuda_if_available());
+        println!("{}", serde_yaml::to_string(&env_config).unwrap());
+        println!("{}", serde_yaml::to_string(&agent_config).unwrap());
+        println!("{}", serde_yaml::to_string(&actor_man_config).unwrap());
+        println!("{}", serde_yaml::to_string(&trainer_config).unwrap());
+    }
+
+    #[derive(Serialize)]
+    pub struct SacMujocoAsyncConfig {
+        pub trainer: AsyncTrainerConfig,
+        pub replay_buffer: SimpleReplayBufferConfig,
+        pub agent: SacConfig<Mlp, Mlp2>,
+    }
+}
+
+mod utils {
+    use super::*;
+
+    pub fn model_dir(args: &Args) -> String {
+        let name = &args.env;
+        format!("./border/examples/mujoco/model/tch/sac_{}_async", name)
+    }
+
+    pub fn create_recorder(
+        args: &Args,
+        config: &config::SacMujocoAsyncConfig,
+    ) -> Result<Box<dyn AggregateRecorder>> {
+        let env_name = &args.env;
+        let (_, _, _, _, model_dir) = env_params(&args);
+        match args.mlflow {
+            true => {
+                let client =
+                    MlflowTrackingClient::new("http://localhost:8080").set_experiment_id("Gym")?;
+                let recorder_run = client.create_recorder("")?;
+                recorder_run.log_params(&config)?;
+                recorder_run.set_tag("env", env_name)?;
+                recorder_run.set_tag("algo", "sac")?;
+                recorder_run.set_tag("backend", "tch")?;
+                Ok(Box::new(recorder_run))
+            }
+            false => Ok(Box::new(TensorboardRecorder::new(model_dir))),
+        }
+    }
+
+    /// Returns (dim_obs, dim_act, target_ent, env_name, model_dir)
+    pub fn env_params<'a>(args: &Args) -> (i64, i64, f64, &'a str, String) {
+        let env_name = &args.env;
+        let model_dir = format!("{}/sac_{}", MODEL_DIR_BASE, env_name);
+        match args.env.as_str() {
+            "ant" => (27, 8, -8., "Ant-v4", model_dir),
+            "cheetah" => (17, 6, -6., "HalfCheetah-v4", model_dir),
+            "walker" => (17, 6, -6., "Walker2d-v4", model_dir),
+            "hopper" => (11, 3, -3., "Hopper-v4", model_dir),
+            env => panic!("Unsupported env {:?}", env),
         }
     }
 }
 
-use config::{agent_config, async_trainer_config, env_config};
+/// Train SAC agent in Mujoco environment
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Name of the environment
+    env: String,
 
-fn parse_args<'a>() -> ArgMatches<'a> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
-    tch::manual_seed(42);
+    /// Create config files
+    #[arg(long, default_value_t = false)]
+    create_config: bool,
 
-    let matches = App::new("dqn_atari_async")
-        .version("0.1.0")
-        .author("Taku Yoshioka <yoshioka@laboro.ai>")
-        .arg(
-            Arg::with_name("show-config")
-                .long("show-config")
-                .takes_value(false)
-                .help("Showing configuration loaded from files"),
-        )
-        .arg(
-            Arg::with_name("n-actors")
-                .long("n-actors")
-                .takes_value(true)
-                .default_value("6")
-                .help("The number of actors"),
-        )
-        .get_matches();
+    /// Show config
+    #[arg(long, default_value_t = false)]
+    show_config: bool,
 
-    matches
+    /// Log metrics with MLflow
+    #[arg(long, default_value_t = false)]
+    mlflow: bool,
+
+    /// Waiting time in milliseconds between frames when evaluation
+    #[arg(long, default_value_t = 25)]
+    wait: u64,
+
+    /// Number of actors, default to 6
+    #[arg(long, default_value_t = 6)]
+    n_actors: usize,
 }
 
-fn show_config(
-    env_config: &EnvConfig,
-    agent_config: &AgentConfig,
-    actor_man_config: &ActorManagerConfig,
-    trainer_config: &AsyncTrainerConfig,
-) {
-    println!("Device: {:?}", tch::Device::cuda_if_available());
-    println!("{}", serde_yaml::to_string(&env_config).unwrap());
-    println!("{}", serde_yaml::to_string(&agent_config).unwrap());
-    println!("{}", serde_yaml::to_string(&actor_man_config).unwrap());
-    println!("{}", serde_yaml::to_string(&trainer_config).unwrap());
-}
+fn train(args: &Args) -> Result<()> {
+    let (dim_obs, dim_act, target_ent, env_name, _model_dir) = utils::env_params(args);
+    let env_config_train = config::env_config(env_name);
+    let model_dir = utils::model_dir(&args);
+    let n_actors = args.n_actors;
 
-fn train(matches: ArgMatches) -> Result<()> {
-    // exploration parameters
-    let n_actors = matches
-        .value_of("n-actors")
-        .unwrap()
-        .parse::<usize>()
-        .unwrap();
-    let env_config_train = env_config();
-    let env_config_eval = env_config();
+    // Configurations
+    let agent_config =
+        config::create_sac_config(dim_obs, dim_act, target_ent).device(cuda_if_available());
     let agent_configs = (0..n_actors)
-        .map(|ix| {
-            agent_config(DIM_OBS, DIM_ACT)
-                .seed(ix as i64)
-                .device(tch::Device::Cpu)
-        })
+        .map(|_| agent_config.clone().device(tch::Device::Cpu))
         .collect::<Vec<_>>();
-    let agent_config = agent_config(DIM_OBS, DIM_ACT).device(tch::Device::cuda_if_available());
+    let env_config_eval = config::env_config(env_name);
     let replay_buffer_config = SimpleReplayBufferConfig::default().capacity(REPLAY_BUFFER_CAPACITY);
     let step_proc_config = SimpleStepProcessorConfig::default();
     let actor_man_config = ActorManagerConfig::default();
-    let async_trainer_config = async_trainer_config();
+    let async_trainer_config = config::create_async_trainer_config(model_dir.as_str())?;
 
-    if matches.is_present("show-config") {
-        show_config(
+    if args.show_config {
+        config::show_config(
             &env_config_train,
             &agent_config,
             &actor_man_config,
             &async_trainer_config,
         );
     } else {
-        let mut recorder = TensorboardRecorder::new(MODEL_DIR);
-        let mut evaluator = Evaluator::new(&env_config_eval, 0, EVAL_EPISODES)?;
+        let mut evaluator = Evaluator::new(&env_config_eval, 0, 1)?;
+        let config = config::SacMujocoAsyncConfig {
+            trainer: async_trainer_config.clone(),
+            replay_buffer: replay_buffer_config.clone(),
+            agent: agent_config.clone(),
+        };
+        let mut recorder = utils::create_recorder(&args, &config)?;
 
-        // Shared flag to stop actor threads
-        let stop = Arc::new(Mutex::new(false));
-
-        // Creates channels
-        let (item_s, item_r) = unbounded(); // items pushed to replay buffer
-        let (model_s, model_r) = unbounded(); // model_info
-
-        // guard for initialization of envs in multiple threads
-        let guard_init_env = Arc::new(Mutex::new(true));
-
-        // Actor manager and async trainer
-        let mut actors = ActorManager::build(
-            &actor_man_config,
+        train_async::<_, Env, ReplayBuffer, StepProc>(
+            &agent_config,
             &agent_configs,
             &env_config_train,
-            &step_proc_config,
-            item_s,
-            model_r,
-            stop.clone(),
-        );
-        let mut trainer = AsyncTrainer::build(
-            &async_trainer_config,
-            &agent_config,
             &env_config_eval,
+            &step_proc_config,
             &replay_buffer_config,
-            item_r,
-            model_s,
-            stop.clone(),
+            &actor_man_config,
+            &async_trainer_config,
+            &mut recorder,
+            &mut evaluator,
         );
-
-        // Set the number of threads
-        tch::set_num_threads(1);
-
-        // Starts sampling and training
-        actors.run(guard_init_env.clone());
-        let stats = trainer.train(&mut recorder, &mut evaluator, guard_init_env);
-        println!("Stats of async trainer");
-        println!("{}", stats.fmt());
-
-        let stats = actors.stop_and_join();
-        println!("Stats of generated samples in actors");
-        println!("{}", actor_stats_fmt(&stats));
     }
 
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let matches = parse_args();
-
-    train(matches)?;
-
+    tch::set_num_threads(1);
+    let args = Args::parse();
+    train(&args)?;
     Ok(())
 }
