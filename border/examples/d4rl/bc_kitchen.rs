7bfbaf7d02f4348afd06457dd134007668043265
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2024-10-12 22:33:12 +0900

    Add arguments for CLI (#26)

--- border/examples/d4rl/bc_kitchen.rs
+++ border/examples/d4rl/bc_kitchen.rs
@@ -9,9 +9,57 @@ use border_core::{
 };
 use border_minari::{d4rl::kitchen::candle::KitchenConverter, MinariDataset};
 use border_mlflow_tracking::MlflowTrackingClient;
+use border_tensorboard::TensorboardRecorder;
 use candle_core::Device;
+use clap::Parser;
+
+/// Train BC agent in kitchen environment
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    // /// Environment name (ant, cheetah, walker, hopper)
+    // #[arg(long)]
+    // env: String,
+
+    // /// Train DQN agent, not evaluate
+    // #[arg(long, default_value_t = false)]
+    // train: bool,
+
+    // /// Evaluate DQN agent, not train
+    // #[arg(long, default_value_t = false)]
+    // eval: bool,
+    // #[arg(long, default_value_t = String::new())]
+    // eval: String,
+
+    // /// Waiting time in milliseconds between frames when evaluation
+    // #[arg(long, default_value_t = 25)]
+    // wait: u64,
+    /// Log metrics with MLflow
+    #[arg(long, default_value_t = false)]
+    mlflow: bool,
+
+    /// The number of optimization steps
+    #[arg(long, default_value_t = 1000)]
+    max_opts: usize,
+
+    /// Interval of evaluation
+    #[arg(long, default_value_t = 100)]
+    eval_interval: usize,
+
+    /// The number of evaluation episodes
+    #[arg(long, default_value_t = 5)]
+    eval_episodes: usize,
+
+    /// Batch size
+    #[arg(long, default_value_t = 256)]
+    batch_size: usize,
+}
+
+const MODEL_DIR: &str = "border/examples/d4rl/model/candle/bc_kitchen";
 
 fn main() -> Result<()> {
+    let args = Args::parse();
+
     let dataset = MinariDataset::load_dataset("D4RL/kitchen/complete-v1", true)?;
 
     // Converter for observation and action
@@ -30,9 +78,11 @@ fn main() -> Result<()> {
     // Create trainer
     let mut trainer = Trainer::build(
         TrainerConfig::default()
-            .max_opts(1000)
-            .eval_interval(100)
-            .model_dir("border/examples/d4rl/model/candle/bc_kitchen"),
+            .max_opts(args.max_opts)
+            .eval_interval(args.eval_interval)
+            .flush_record_interval(args.max_opts / 50)
+            .record_agent_info_interval(args.max_opts / 50)
+            .model_dir(MODEL_DIR),
     );
 
     // Create behavior cloning agent
@@ -48,6 +98,7 @@ fn main() -> Result<()> {
         };
         BcConfig::<Mlp>::default()
             .policy_model_config(policy_model_config)
+            .batch_size(args.batch_size)
             .device(Device::Cpu)
             .action_type(BcActionType::Continuous)
     };
@@ -55,18 +106,23 @@ fn main() -> Result<()> {
 
     // Create recorder
     let mut recorder: Box<dyn AggregateRecorder> = {
-        let client =
-            MlflowTrackingClient::new("http://localhost:8080").set_experiment_id("D4RL")?;
-        let recorder_run = client.create_recorder("")?;
-        recorder_run.log_params(&agent_config)?;
-        recorder_run.set_tag("env", "kitchen")?;
-        recorder_run.set_tag("algo", "bc")?;
-        recorder_run.set_tag("backend", "candle")?;
-        Box::new(recorder_run)
+        match args.mlflow {
+            true => {
+                let client =
+                    MlflowTrackingClient::new("http://localhost:8080").set_experiment_id("D4RL")?;
+                let recorder_run = client.create_recorder("")?;
+                recorder_run.log_params(&agent_config)?;
+                recorder_run.set_tag("env", "kitchen")?;
+                recorder_run.set_tag("algo", "bc")?;
+                recorder_run.set_tag("backend", "candle")?;
+                Box::new(recorder_run)
+            }
+            false => Box::new(TensorboardRecorder::new(MODEL_DIR)),
+        }
     };
 
     // Create evaluator
-    let mut evaluator = DefaultEvaluator::new(env, 5)?;
+    let mut evaluator = DefaultEvaluator::new(env, args.eval_episodes)?;
 
     // Start training
     let _ = trainer.train_offline(&mut agent, &mut buffer, &mut recorder, &mut evaluator);
