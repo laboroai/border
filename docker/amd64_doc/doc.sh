9e67a0a4cfd7dbed1faaf62d8d630dce646cd94d
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2024-10-12 22:33:28 +0900

    Update docker files

--- docker/amd64_doc/doc.sh
+++ docker/amd64_doc/doc.sh
@@ -1,6 +1,2 @@
-docker run -it --rm \
-    --name border_doc \
-    --shm-size=512m \
-    --volume="$(pwd)/../..:/home/ubuntu/border" \
-    border_doc bash -l -c \
-    "cd /home/ubuntu/border; source /root/venv/bin/activate; LIBTORCH_USE_PYTORCH=1 CARGO_TARGET_DIR=/home/ubuntu/target cargo doc --no-deps --document-private-items; cp -r /home/ubuntu/target/doc ."
+docker exec border_doc bash -l -c \
+    "cd /home/ubuntu/border; source /root/venv/bin/activate; LIBTORCH_USE_PYTORCH=1 LD_LIBRARY_PATH=$HOME/venv/lib/python3.10/site-packages/torch/lib:$LD_LIBRARY_PATH CARGO_TARGET_DIR=/home/ubuntu/target cargo doc --no-deps --document-private-items --features=candle; cp -r /home/ubuntu/target/doc ."
