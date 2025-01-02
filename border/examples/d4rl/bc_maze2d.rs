c8034b8c6450aa5fc406a0bd7aff399ec12841e8
taku-y <taku.yoshioka.4096@gmail.com>
2025-01-02 14:42:18 +0900

    Tweak (#26)

--- border/examples/d4rl/bc_maze2d.rs
+++ border/examples/d4rl/bc_maze2d.rs
@@ -21,26 +21,21 @@ use border_mlflow_tracking::MlflowTrackingClient;
 use border_tensorboard::TensorboardRecorder;
 use candle_core::{Device, Tensor};
 use clap::Parser;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
+const MODEL_DIR: &str = "border/examples/d4rl/model/candle/bc_maze2d";
+const MLFLOW_EXPERIMENT_NAME: &str = "D4RL";
+const MLFLOW_RUN_NAME: &str = "bc_maze2d_candle";
+const MLFLOW_TAGS: &[(&str, &str)] = &[("env", "maze2d"), ("algo", "sac"), ("backend", "candle")];
+
 /// Train BC agent in maze2d environment
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Serialize, Deserialize)]
 #[command(version, about)]
 struct Args {
-    // /// Environment name (ant, cheetah, walker, hopper)
-    // #[arg(long)]
-    // env: String,
-
-    // /// Train DQN agent, not evaluate
-    // #[arg(long, default_value_t = false)]
-    // train: bool,
-
-    // /// Evaluate DQN agent, not train
-    // #[arg(long, default_value_t = false)]
-    // eval: bool,
-    // #[arg(long, default_value_t = String::new())]
-    // eval: String,
+    /// train or eval
+    #[arg(long)]
+    mode: String,
 
     // /// Waiting time in milliseconds between frames when evaluation
     // #[arg(long, default_value_t = 25)]
@@ -70,20 +65,66 @@ struct Args {
     batch_size: usize,
 }
 
-const MODEL_DIR: &str = "border/examples/d4rl/model/candle/bc_maze2d";
+#[derive(Serialize)]
+struct BcMaze2dConfig {
+    args: Args,
+    trainer_config: TrainerConfig,
+    agent_config: BcConfig<Mlp>,
+}
 
-fn create_trainer(args: &Args) -> Trainer {
-    log::info!("Create trainer");
-    Trainer::build(
-        TrainerConfig::default()
+impl BcMaze2dConfig {
+    fn new(args: Args) -> Self {
+        let trainer_config = TrainerConfig::default()
             .max_opts(args.max_opts)
             .eval_interval(args.eval_interval)
             .flush_record_interval(args.max_opts / 50)
-            .record_agent_info_interval(args.max_opts / 50),
-    )
+            .record_agent_info_interval(args.max_opts / 50);
+
+        let agent_config = {
+            // Dimensions of observation and action
+            let dim_obs = match args.include_goal {
+                true => 6,
+                false => 4,
+            };
+            let dim_act = 2;
+
+            let policy_model_config = {
+                let policy_model_config = MlpConfig {
+                    in_dim: dim_obs,
+                    out_dim: dim_act,
+                    units: vec![256, 256],
+                    activation_out: Activation::Tanh,
+                };
+                BcModelConfig::default().policy_model_config(policy_model_config)
+            };
+            BcConfig::<Mlp>::default()
+                .policy_model_config(policy_model_config)
+                .batch_size(args.batch_size)
+                .device(Device::Cpu)
+                .action_type(BcActionType::Continuous)
+                .optimizer(border_candle_agent::opt::OptimizerConfig::AdamW {
+                    lr: 0.0003,
+                    beta1: candle_nn::ParamsAdamW::default().beta1,
+                    beta2: candle_nn::ParamsAdamW::default().beta2,
+                    eps: candle_nn::ParamsAdamW::default().eps,
+                    weight_decay: candle_nn::ParamsAdamW::default().weight_decay,
+                })
+        };
+
+        Self {
+            args,
+            trainer_config,
+            agent_config,
+        }
+    }
 }
 
-fn create_agent<E, R>(args: &Args) -> (Box<dyn Agent<E, R>>, impl Serialize)
+fn create_trainer(config: &BcMaze2dConfig) -> Trainer {
+    log::info!("Create trainer");
+    Trainer::build(config.trainer_config.clone())
+}
+
+fn create_agent<E, R>(config: &BcMaze2dConfig) -> Box<dyn Agent<E, R>>
 where
     E: Env + 'static,
     E::Obs: Into<Tensor>,
@@ -93,39 +134,8 @@ where
     <R::Batch as TransitionBatch>::ObsBatch: Into<Tensor>,
     <R::Batch as TransitionBatch>::ActBatch: Into<Tensor>,
 {
-    // Dimensions of observation and action
-    let dim_obs = match args.include_goal {
-        true => 6,
-        false => 4,
-    };
-    let dim_act = 2;
-
     log::info!("Create agent");
-    let agent_config = {
-        let policy_model_config = {
-            let policy_model_config = MlpConfig {
-                in_dim: dim_obs,
-                out_dim: dim_act,
-                units: vec![256, 256],
-                activation_out: Activation::Tanh,
-            };
-            BcModelConfig::default().policy_model_config(policy_model_config)
-        };
-        BcConfig::<Mlp>::default()
-            .policy_model_config(policy_model_config)
-            .batch_size(args.batch_size)
-            .device(Device::Cpu)
-            .action_type(BcActionType::Continuous)
-            .optimizer(border_candle_agent::opt::OptimizerConfig::AdamW {
-                lr: 0.0003,
-                beta1: candle_nn::ParamsAdamW::default().beta1,
-                beta2: candle_nn::ParamsAdamW::default().beta2,
-                eps: candle_nn::ParamsAdamW::default().eps,
-                weight_decay: candle_nn::ParamsAdamW::default().weight_decay,
-            })
-    };
-
-    (Box::new(Bc::build(agent_config.clone())), agent_config)
+    Box::new(Bc::build(config.agent_config.clone()))
 }
 
 fn create_replay_buffer<T>(
@@ -144,23 +154,20 @@ where
 }
 
 fn create_recorder<E, R>(
-    args: &Args,
-    agent_config: &impl Serialize,
+    config: &BcMaze2dConfig,
 ) -> Result<Box<dyn Recorder<E, R>>>
 where
     E: Env + 'static,
     R: ReplayBufferBase + 'static,
 {
     log::info!("Create recorder");
-    match args.mlflow {
+    match config.args.mlflow {
         true => {
-            let client =
-                MlflowTrackingClient::new("http://localhost:8080").set_experiment("D4RL")?;
-            let recorder_run = client.create_recorder("")?;
-            recorder_run.log_params(&agent_config)?;
-            recorder_run.set_tag("env", "maze2d")?;
-            recorder_run.set_tag("algo", "bc")?;
-            recorder_run.set_tag("backend", "candle")?;
+            let client = MlflowTrackingClient::new("http://localhost:8080")
+                .set_experiment(MLFLOW_EXPERIMENT_NAME)?;
+            let recorder_run = client.create_recorder(MLFLOW_RUN_NAME)?;
+            recorder_run.log_params(config)?;
+            recorder_run.set_tags(MLFLOW_TAGS)?;
             Ok(Box::new(recorder_run))
         }
         false => Ok(Box::new(TensorboardRecorder::new(
@@ -183,7 +190,7 @@ where
     PointMazeEvaluator::new(env, args.eval_episodes)
 }
 
-fn train<T>(args: Args, dataset: MinariDataset, converter: T) -> Result<()>
+fn train<T>(config: BcMaze2dConfig, dataset: MinariDataset, converter: T) -> Result<()>
 where
     T: MinariConverter + 'static,
     T::Obs: std::fmt::Debug + Into<Tensor>,
@@ -191,11 +198,11 @@ where
     T::ObsBatch: std::fmt::Debug + Into<Tensor> + 'static,
     T::ActBatch: std::fmt::Debug + Into<Tensor> + 'static,
 {
-    let mut trainer = create_trainer(&args);
-    let (mut agent, agent_config) = create_agent(&args);
+    let mut trainer = create_trainer(&config);
+    let mut agent = create_agent(&config);
     let mut buffer = create_replay_buffer(&converter, &dataset)?;
-    let mut recorder = create_recorder(&args, &agent_config)?;
-    let mut evaluator = create_evaluator(&args, converter, &dataset)?;
+    let mut recorder = create_recorder(&config)?;
+    let mut evaluator = create_evaluator(&config.args, converter, &dataset)?;
 
     log::info!("Start training");
     let _ = trainer.train_offline(&mut agent, &mut buffer, &mut recorder, &mut evaluator);
@@ -207,11 +214,12 @@ fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let args = Args::parse();
 
+    let config = BcMaze2dConfig::new(args);
     let dataset = MinariDataset::load_dataset("D4RL/pointmaze/umaze-v2", true)?;
     let converter = PointMazeConverter::new(PointMazeConverterConfig {
         // Include goal position in observation
-        include_goal: args.include_goal,
+        include_goal: config.args.include_goal,
     });
 
-    train(args, dataset, converter)
+    train(config, dataset, converter)
 }
