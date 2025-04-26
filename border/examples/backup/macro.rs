3b01c8ab3324eea0765f4dc96329f233ef2b7010
taku-y <taku.yoshioka.4096@gmail.com>
2025-04-26 11:40:39 +0900

    Remove old examples

--- border/examples/backup/macro.rs
+++
@@ -1,9 +0,0 @@
-use border_core::shape;
-use border_py_gym_env::{newtype_act_d, newtype_obs};
-
-shape!(ObsShape, [1, 2, 3]);
-newtype_obs!(Obs, ObsFilter, ObsShape, f32, f32);
-newtype_act_d!(ActD, ActDFilter);
-newtype_act_d!(ActC, ActCFilter);
-
-fn main() {}
